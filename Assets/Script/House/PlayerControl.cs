d703842ce9806c3ac1958837ec711444b5c909c9
GanXiKang <gan0612xk@hotmail.com>
2024-07-14 21:01:18 +0800

    feat ： 編寫code

--- Assets/Script/House/PlayerControl.cs
+++ Assets/Script/House/PlayerControl.cs
@@ -5,13 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    CharacterController cc;
+
+    private Vector3 _moveInput;
+
+    public float _moveSpeed = 7f;
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+        cc.Move(_moveInput * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        _moveInput = new Vector3(input.x, 0f, input.y);
     }
 }
