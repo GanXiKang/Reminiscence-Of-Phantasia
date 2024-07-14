f8df0b1b41be66fabf3b6c9bd5909d822b0dd9c0
GanXiKang <gan0612xk@hotmail.com>
2024-07-14 21:59:03 +0800

    feat : 編寫camera位置的判斷

--- Assets/Script/House/PlayerControl.cs
+++ Assets/Script/House/PlayerControl.cs
@@ -18,7 +18,13 @@ public class PlayerControl : MonoBehaviour
 
     void FixedUpdate()
     {
-        cc.Move(_moveInput * _moveSpeed * Time.fixedDeltaTime);
+        Vector3 cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+        Vector3 movement = cameraForward * _moveInput.y + Camera.main.transform.right * _moveInput.x;
+        print(movement);
+
+        cc.Move(movement * _moveSpeed * Time.fixedDeltaTime);
     }
 
     void OnMove(InputValue value)
