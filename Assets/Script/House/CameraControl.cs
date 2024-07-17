262c18194475caf8acc522b66d79f78547665ce6
GanXiKang <gan0612xk@hotmail.com>
2024-07-17 20:58:20 +0800

    feat : 編寫code

--- Assets/Script/House/CameraControl.cs
+++ Assets/Script/House/CameraControl.cs
@@ -8,6 +8,13 @@ public class CameraControl : MonoBehaviour
     public GameObject freeLookCamera;
     public static bool isFreeLook;
 
+    [Header("CameraPosition")]
+    public Transform startPos;
+    public Transform workbenchPos;
+    public static bool isLookWorkbench;
+    float _moveTime = 1f;
+    bool once = false;
+
     void Start()
     {
         isFreeLook = false;
@@ -17,6 +24,21 @@ public class CameraControl : MonoBehaviour
     void Update()
     {
         freeLookCamera.SetActive(isFreeLook);
+
+        if (isLookWorkbench)
+        {
+            once = true;
+            transform.position = Vector3.Lerp(transform.position, workbenchPos.position, _moveTime * Time.deltaTime); ;
+            transform.rotation = Quaternion.Lerp(transform.rotation, workbenchPos.rotation, _moveTime * Time.deltaTime);
+        }
+        else
+        {
+            if (once)
+            {
+                transform.position = startPos.position;
+                transform.rotation = startPos.rotation;
+            }
+        }
     }
 
     void StartFreeLookCamera()
