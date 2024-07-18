a2321944b3cdd1099c428c3aec1b37728af1aa9e
GanXiKang <gan0612xk@hotmail.com>
2024-07-18 23:42:00 +0800

    ｆｅａｔ　：　ｃｏｐｙ　ｃｏｄｅ

--- Assets/Script/House/CameraControl_House.cs
+++ Assets/Script/House/CameraControl_House.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CameraControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("FreeLookCamera")]
+    public GameObject freeLookCamera;
+    public static bool isFreeLook;
+
+    [Header("CameraPosition")]
+    public Transform startPos;
+    public Transform workbenchPos;
+    public static bool isLookWorkbench;
+    float _moveTime = 1f;
+    bool once = false;
+
     void Start()
     {
-        
+        isFreeLook = false;
+        isLookWorkbench = false;
+        Invoke("StartFreeLookCamera", 2f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        freeLookCamera.SetActive(isFreeLook);
+
+        if (isLookWorkbench)
+        {
+            once = true;
+            transform.position = Vector3.Lerp(transform.position, workbenchPos.position, _moveTime * Time.deltaTime); ;
+            transform.rotation = Quaternion.Lerp(transform.rotation, workbenchPos.rotation, _moveTime * Time.deltaTime);
+        }
+        else
+        {
+            if (once)
+            {
+                transform.position = startPos.position;
+                transform.rotation = startPos.rotation;
+            }
+        }
+    }
+
+    void StartFreeLookCamera()
+    {
+        isFreeLook = true;
     }
 }
