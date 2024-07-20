eb21599a6fc9f17697078bfd32430f8a641bffab
GanXiKang <gan0612xk@hotmail.com>
2024-07-21 01:10:18 +0800

    feat : copy code

--- Assets/Script/House/ColliderControl_House.cs
+++ Assets/Script/House/ColliderControl_House.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ColliderControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int _serialNumber;
+    public static int _nowNumber;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            if (//!GameControl_House.isNextPlace)
+            {
+                //UIControl_House.isInteractionButtonActive = true;
+                //UIControl_House._conveyColliderNumber = _serialNumber;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            _nowNumber = _serialNumber;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //UIControl_House.isInteractionButtonActive = false;
+        }
     }
 }
