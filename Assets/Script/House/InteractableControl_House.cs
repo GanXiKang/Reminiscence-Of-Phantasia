7e6f96a4a5086517f798c5b6fea024d0569592de
GanXiKang <gan0612xk@hotmail.com>
2024-07-21 01:26:07 +0800

    feat ： 編寫code

--- Assets/Script/House/InteractableControl_House.cs
+++ Assets/Script/House/InteractableControl_House.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class InteractableControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("InteractableUI")]
+    public GameObject interactableUI;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
