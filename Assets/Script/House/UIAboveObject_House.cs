a287e32e7e69a433ab05642f01973c16a58d1ad0
GanXiKang <gan0612xk@hotmail.com>
2024-07-20 23:43:02 +0800

    feat : copy 過來

--- Assets/Script/House/UIAboveObject_House.cs
+++ Assets/Script/House/UIAboveObject_House.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIAboveObject_House : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Workbench")]
+    public Transform workbench;
+    public RectTransform workbenchHint;
+    public Vector3 workbenchOffset;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 Pos = workbench.position + workbenchOffset;
+        workbenchHint.position = Pos;
     }
 }
