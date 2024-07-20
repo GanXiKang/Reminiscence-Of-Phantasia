d32f349c4758a494159bb98ea79c647a8799668a
GanXiKang <gan0612xk@hotmail.com>
2024-07-21 02:26:56 +0800

    feat ： ｎｅｗ　ｓｃｒｉｐｔ

---
+++ Assets/Script/House/WorkbenchControl_House.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkbenchControl_House : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
