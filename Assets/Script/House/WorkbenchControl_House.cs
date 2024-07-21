4134de2b43694ab68bc7f90d1232022afa8b10ef
GanXiKang <gan0612xk@hotmail.com>
2024-07-21 17:13:19 +0800

    feat ： 編寫流程

--- Assets/Script/House/WorkbenchControl_House.cs
+++ Assets/Script/House/WorkbenchControl_House.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class WorkbenchControl_House : MonoBehaviour
 {
+    int _process = 0;
+
     void Update()
     {
+        switch (_process)
+        {
+            case 1:
+                Step1_Choose();
+                break;
+            case 2:
+                Step2_Cut();
+                break;
+            case 3:
+                Step3_Color();
+                break;
+            case 4:
+                Step4_Install();
+                break;
+        }
         Leave();
-        
     }
 
-    //void Step1_
+    void Step1_Choose()
+    {
+        
+    }
+    void Step2_Cut()
+    {
 
+    }
+    void Step3_Color()
+    {
+
+    }
+    void Step4_Install()
+    {
+
+    }
     void Leave()
     {
         if (CameraControl_House.isLookWorkbench)
