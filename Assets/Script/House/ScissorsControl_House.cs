55e3a7c10d066b58774c91338b8b10d5dd106a8d
GanXiKang <gan0612xk@hotmail.com>
2024-07-24 20:53:14 +0800

    feat ： 搬code

--- Assets/Script/House/ScissorsControl_House.cs
+++ Assets/Script/House/ScissorsControl_House.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class ScissorsControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float mouseY;
+    float mouseX;
+    float _moveSpeed = 12f;
+    float minY = -3f, maxY = 4f;
+    float minX = 0f, maxX = 3f;
+    public static bool isUseScissors = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (WorkbenchControl_House._process == 2)
+        {
+            ScissorsMove();
+            if (Input.GetMouseButtonDown(0))
+            {
+                isUseScissors = true;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                isUseScissors = false;
+            }
+            if (isUseScissors)
+            {
+                print("Cut!");
+            }
+        }
+    }
+    void ScissorsMove()
+    {
+        mouseY = Input.GetAxis("Mouse Y") * _moveSpeed * Time.deltaTime;
+        mouseX = Input.GetAxis("Mouse X") * _moveSpeed * Time.deltaTime;
+        float newY = transform.position.y + mouseY;
+        float newX = transform.position.x + mouseX;
+        newY = Mathf.Clamp(newY, minY, maxY);
+        newX = Mathf.Clamp(newX, minX, maxX);
+        transform.position = new Vector3(newX, newY, 0f);
     }
 }
