be56a785403ca886c8bad03a4ccc586d95e3d3e8
GanXiKang <gan0612xk@hotmail.com>
2024-07-30 02:05:59 +0800

    feat : new script

---
+++ Assets/Script/PublicStory/StoryBagControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryBagControl : MonoBehaviour
+{
+    bool isOpenBag = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public 
+}
