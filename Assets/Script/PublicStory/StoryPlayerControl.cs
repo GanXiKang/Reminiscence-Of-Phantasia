e80bb0fd0e06972787a41889810821b7a06aef91
GanXiKang <gan0612xk@hotmail.com>
2024-07-29 23:39:52 +0800

    feat : copy code

--- Assets/Script/PublicStory/StoryPlayerControl.cs
+++ Assets/Script/PublicStory/StoryPlayerControl.cs
@@ -1,17 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class StoryPlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController cc;
+    PlayerInput playerInput;
+
+    private Vector3 _storyMoveInput;
+
+    [Header("Movement")]
+    public float _moveSpeed = 10f;
+    public float _gravity = 20f;
+
+    float _direction = 1;
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _moveSpeed = 15f;
+        }
+        else
+        {
+            _moveSpeed = 8f;
+        }
+
+        if (!cc.isGrounded)
+        {
+            _storyMoveInput.y -= _gravity * Time.fixedDeltaTime;
+        }
+
+        cc.Move(_storyMoveInput * _moveSpeed * Time.deltaTime);
+    }
+
+    void OnMove(InputValue value)
     {
         
     }
