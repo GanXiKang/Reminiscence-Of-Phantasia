ded17aca54c8e8b188e0b2c7c781fa990ec483fe
GanXiKang <gan0612xk@hotmail.com>
2024-08-02 02:59:16 +0800

    feat ： debug

--- Assets/Script/PublicStory/StoryPlayerControl.cs
+++ Assets/Script/PublicStory/StoryPlayerControl.cs
@@ -24,25 +24,16 @@ public class StoryPlayerControl : MonoBehaviour
     {
         StoryPlayerMove();
         StoryPlayerOnTheGround();
-
-        //if (Input.GetKeyDown(KeyCode.T))
-        //{
-        //    playerInput.enabled = !playerInput.enabled;
-        //}
     }
 
     void OnMove(InputValue value)
     {
         Vector2 input = value.Get<Vector2>();
-        _storyMoveInput = new Vector3(input.y, 0f, input.x);
+        _storyMoveInput = new Vector3(-input.y, 0f, input.x);
     }
 
     void StoryPlayerMove()
     {
-        Quaternion rotation = transform.rotation;
-        print(rotation);
-        //Vector3 adjustedMoveInput = rotation * _storyMoveInput;
-        //cc.Move((_storyVelocity + adjustedMoveInput * _moveSpeed) * Time.fixedDeltaTime);
         cc.Move((_storyVelocity + _storyMoveInput * _moveSpeed) * Time.fixedDeltaTime);
     }
     void StoryPlayerOnTheGround()
