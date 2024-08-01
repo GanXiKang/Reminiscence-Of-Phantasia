0e0c7981a21562dcacddc2ca15d3b29bfa5d714c
GanXiKang <gan0612xk@hotmail.com>
2024-08-02 02:05:47 +0800

    feat ： 添加内容

--- Assets/Script/Story_Girl/StoryItemIntroduce_Girl.cs
+++ Assets/Script/Story_Girl/StoryItemIntroduce_Girl.cs
@@ -5,16 +5,28 @@ using UnityEngine.UI;
 
 public class StoryItemIntroduce_Girl : MonoBehaviour
 {
-    //[Header("Introduce")]
-    //public GameObject background;
+    [Header("IntroduceUI")]
+    public GameObject introduce;
+    public GameObject panel;
+    public Text content;
+    string[] introduceItem = new string[10];
+
+    void Start()
+    {
+        
+    }
 
     public void OnPointEnter(int _whichItem)
     {
         print("OK");
     }
-
     public void OnPointExit()
     {
         print("Yes");
+    }
+
+    void ItemIntroduceContent()
+    {
+        introduceItem[1] = 
     }
 }
