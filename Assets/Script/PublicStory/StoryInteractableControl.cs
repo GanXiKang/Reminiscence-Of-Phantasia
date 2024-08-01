01b4b91bde1693ce986eb7086d17d5a2f8ed8e8e
GanXiKang <gan0612xk@hotmail.com>
2024-08-01 22:33:05 +0800

    feat ： static

--- Assets/Script/PublicStory/StoryInteractableControl.cs
+++ Assets/Script/PublicStory/StoryInteractableControl.cs
@@ -14,8 +14,9 @@ public class StoryInteractableControl : MonoBehaviour
     [Header("Item")]
     public int _giveItemNumber;
     public int _getItemNumber;
+    public static bool isGetItem = false;
     bool isGiveItem = false;
-    bool isGetItem = false;
+
 
     void Start()
     {
