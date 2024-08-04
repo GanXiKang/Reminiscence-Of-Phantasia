0733e8518755819407c30b14f93b908934b7e21f
GanXiKang <gan0612xk@hotmail.com>
2024-08-05 00:50:53 +0800

    feat ： copy code

--- Assets/Script/Workbench/LoadPaperColor_Workbench.cs
+++ Assets/Script/Workbench/LoadPaperColor_Workbench.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LoadPaperColor_Workbench : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Renderer[] canvasRenderers;
+    public string loadDirectory;
+
     void Start()
     {
-        
+        loadDirectory = Application.persistentDataPath + "/Artwork.png";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadTexture()
     {
-        
+        for (int i = 0; i < canvasRenderers.Length; i++)
+        {
+            string filePath = Path.Combine(loadDirectory, $"Artwork_{i}.png");
+
+            if (File.Exists(filePath))
+            {
+                byte[] bytes = File.ReadAllBytes(filePath);
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(bytes);
+                canvasRenderers[i].material.mainTexture = texture;
+                Debug.Log("Artwork loaded from " + filePath);
+            }
+            else
+            {
+                Debug.LogWarning("File not found: " + filePath);
+            }
+        }
     }
 }
