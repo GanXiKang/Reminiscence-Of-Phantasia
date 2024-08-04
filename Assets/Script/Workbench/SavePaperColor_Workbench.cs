84f3cfc9e5c69a7559a2b162c9672616a1bd1764
GanXiKang <gan0612xk@hotmail.com>
2024-08-04 23:38:49 +0800

    feat : copy code

--- Assets/Script/Workbench/SavePaperColor_Workbench.cs
+++ Assets/Script/Workbench/SavePaperColor_Workbench.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SavePaperColor_Workbench : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Renderer[] canvasRenderers;
+    public string saveDirectory;
+
     void Start()
     {
-        
+        saveDirectory = Application.persistentDataPath + "/Artwork.png";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveTexture()
     {
-        
+        if (!Directory.Exists(saveDirectory))
+        {
+            Directory.CreateDirectory(saveDirectory);
+        }
+
+        for (int i = 0; i < canvasRenderers.Length; i++)
+        {
+            Texture2D texture = (Texture2D)canvasRenderers[i].material.mainTexture;
+            byte[] bytes = texture.EncodeToPNG();
+            string filePath = Path.Combine(saveDirectory, $"Artwork_{i}.png");
+            File.WriteAllBytes(filePath, bytes);
+            Debug.Log("Artwork saved to " + filePath);
+        }
     }
 }
