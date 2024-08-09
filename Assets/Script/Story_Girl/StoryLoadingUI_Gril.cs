a7fa4d38e76b52d347c4755ca7a2c9d375bc4d64
GanXiKang <gan0612xk@hotmail.com>
2024-08-10 02:28:31 +0800

    feat : 編寫code

--- Assets/Script/Story_Girl/StoryLoadingUI_Gril.cs
+++ Assets/Script/Story_Girl/StoryLoadingUI_Gril.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryLoadingUI_Gril : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("LoadingUI")]
+    public GameObject loadingUI;
+    public Image a,b,c;
+    public static bool isLeft, isRight;
+
     void Start()
+    {
+        isLeft = false;
+        isRight = false;
+    }
+
+    void Update()
+    {
+        LeftSwitchScene();
+        RightSwitchScene();
+    }
+
+    void LeftSwitchScene()
+    {
+        if (isLeft)
+        {
+            a.fillOrigin = (int)Image.OriginHorizontal.Left;
+            b.fillOrigin = 0;
+            c.fillOrigin = 0;
+        }
+    }
+    void RightSwitchScene()
+    {
+        if (isRight)
+        {
+            a.fillOrigin = (int)Image.OriginHorizontal.Right;
+            b.fillOrigin = 1;
+            c.fillOrigin = 1;
+        }
+    }
+    void A_BarValue()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void B_BarValue()
     {
-        
+
+    }
+    void C_BarValue()
+    {
+
     }
 }
