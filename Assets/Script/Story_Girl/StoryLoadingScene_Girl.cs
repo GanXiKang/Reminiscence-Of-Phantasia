5ca5041b4eff619d772ef63de4ad433c545edc3a
GanXiKang <gan0612xk@hotmail.com>
2024-08-11 01:53:01 +0800

    feat ： 搬code

--- Assets/Script/Story_Girl/StoryLoadingScene_Girl.cs
+++ Assets/Script/Story_Girl/StoryLoadingScene_Girl.cs
@@ -1,18 +1,160 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryLoadingScene_Girl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Scene")]
+    public GameObject street;
+    public GameObject forest;
 
-    // Update is called once per frame
+    [Header("LoadingUI")]
+    public GameObject loadingUI;
+    public Image a, b, c;
+    public static bool isLoading = false;
+    public static bool isLeftOpen = false;     //ªÿ»•
+    public static bool isLeftClose = false;
+    public static bool isRightOpen = false;    //«∞ﬂM
+    public static bool isRightClose = false;
+    float _loadingSpeed = 1.5f;
+
     void Update()
     {
-        
+        loadingUI.SetActive(isLoading);
+
+        LeftSwitch();
+        RightSwitch();
+
+        if (Input.GetKeyDown(KeyCode.L))  //úy‘á
+        {
+            isRightOpen = true;
+        }
+        if (Input.GetKeyDown(KeyCode.K))  //úy‘á
+        {
+            isLeftOpen = true;
+        }
     }
+
+    void LeftSwitch()
+    {
+        if (isLeftOpen)
+        {
+            StartCoroutine(LeftSwitchScene_Open());
+        }
+        if (isLeftClose)
+        {
+            StartCoroutine(LeftSwitchScene_Close());
+        }
+    }
+    void RightSwitch()
+    {
+        if (isRightOpen)
+        {
+            StartCoroutine(RightSwitchScene_Open());
+        }
+        if (isRightClose)
+        {
+            StartCoroutine(RightSwitchScene_Close());
+        }
+    }
+    void BarValue(Image bar, bool isAdd)
+    {
+        if (isAdd)
+        {
+            if (bar.fillAmount < 1)
+            {
+                bar.fillAmount += _loadingSpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (bar.fillAmount > 0)
+            {
+                bar.fillAmount -= _loadingSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator LeftSwitchScene_Open()
+    {
+        isLoading = true;
+        a.fillOrigin = 0; //(int)Image.OriginHorizontal.Left
+        b.fillOrigin = 0;
+        c.fillOrigin = 0;
+
+        BarValue(a, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(c, true);
+
+        if (c.fillAmount == 1)
+        {
+            isLeftOpen = false;
+            yield return new WaitForSeconds(0.5f);
+            street.SetActive(true);
+            forest.SetActive(false);
+            isRightClose = true;
+        }
+    }
+    IEnumerator LeftSwitchScene_Close()
+    {
+        a.fillOrigin = 0;
+        b.fillOrigin = 0;
+        c.fillOrigin = 0;
+
+        BarValue(c, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(a, false);
+
+        if (a.fillAmount == 0)
+        {
+            isLeftClose = false;
+            isLoading = false;
+        }
+    }
+    IEnumerator RightSwitchScene_Open()
+    {
+        isLoading = true;
+        a.fillOrigin = 1;
+        b.fillOrigin = 1;
+        c.fillOrigin = 1;
+
+        BarValue(a, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(c, true);
+
+        if (c.fillAmount == 1)
+        {
+            isRightOpen = false;
+            yield return new WaitForSeconds(0.5f);
+            forest.SetActive(true);
+            street.SetActive(false);
+            isLeftClose = true;
+        }
+    }
+    IEnumerator RightSwitchScene_Close()
+    {
+        a.fillOrigin = 1;
+        b.fillOrigin = 1;
+        c.fillOrigin = 1;
+
+        BarValue(c, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(a, false);
+
+        if (a.fillAmount == 0)
+        {
+            isRightClose = false;
+            isLoading = false;
+        }
+    }
+}
 }
