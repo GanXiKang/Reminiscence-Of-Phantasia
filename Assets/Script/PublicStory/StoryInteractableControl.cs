8923f682c87b3ea8bf32ed9f1a46616f4590acaf
GanXiKang <gan0612xk@hotmail.com>
2024-08-12 00:20:37 +0800

    feat : 測試

--- Assets/Script/PublicStory/StoryInteractableControl.cs
+++ Assets/Script/PublicStory/StoryInteractableControl.cs
@@ -76,6 +76,7 @@ public class StoryInteractableControl : MonoBehaviour
     {
         if (!isPickedUp || isAnim) return;
 
+        isPlayerMove = false;
         moveItemUI.SetActive(true);
         Vector3 startPosition = Camera.main.WorldToScreenPoint(transform.position) + uiOffset;
         moveItemUI.transform.position = startPosition;
@@ -135,7 +136,6 @@ public class StoryInteractableControl : MonoBehaviour
     IEnumerator MoveItemUI(GameObject itemUI, Vector3 start, Vector3 end)
     {
         isAnim = true;
-        isPlayerMove = false;
         float duration = 1f;
         float elapsed = 0f;
 
