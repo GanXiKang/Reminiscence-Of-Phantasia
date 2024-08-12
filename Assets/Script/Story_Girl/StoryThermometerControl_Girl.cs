698c20a143628685680e77152a3956c557817413
GanXiKang <gan0612xk@hotmail.com>
2024-08-13 00:08:27 +0800

    feat : 搬code

--- Assets/Script/Story_Girl/StoryThermometerControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryThermometerControl_Girl.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryThermometerControl_Girl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool isThermometer = false;
+    public static bool isDead = false;
+    float _temperature = 36.5f;
+    float _decline = 0.1f;
+    float _rise = 0.15f;
 
-    // Update is called once per frame
+    [Header("UI")]
+    public GameObject thermometerUI;
+    public Image energyBar;
+    public Text temperature;
+
     void Update()
     {
-        
+        Thermometer();
+        Limit();
+    }
+
+    void Thermometer()
+    {
+        thermometerUI.SetActive(isThermometer);
+        energyBar.fillAmount = (_temperature - 35f) / 2;
+        temperature.text = _temperature.ToString("F1");
+
+        if (isThermometer)
+        {
+            //if (!StoryGameControl_LittleGirl.isUseMatches)
+            //{
+            //    _temperature -= _decline * Time.deltaTime;
+            //}
+            //else
+            //{
+            //    _temperature += _rise * Time.deltaTime;
+            //}
+        }
+    }
+    void Limit()
+    {
+        if (_temperature >= 37.0f)
+        {
+            _temperature = 37.0f;
+        }
+
+        if (_temperature <= 35.0f)
+        {
+            _temperature = 35.0f;
+            isDead = true;
+        }
     }
 }
