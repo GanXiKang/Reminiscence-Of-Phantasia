e7b03b7ed4d7b08fbe1c8d7ba1b9a5ab123e463d
GanXiKang <gan0612xk@hotmail.com>
2024-08-15 18:45:05 +0800

    feat ： delete

--- Assets/Script/PublicStory/StoryBagControl.cs
+++ Assets/Script/PublicStory/StoryBagControl.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class StoryBagControl : MonoBehaviour
 {
