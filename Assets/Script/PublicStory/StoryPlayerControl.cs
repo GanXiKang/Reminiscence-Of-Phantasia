87800631dbb24a9d45b0ce8d75b3bdeed19f06b4
GanXiKang <gan0612xk@hotmail.com>
2024-08-15 18:07:24 +0800

    feat ： 優化

--- Assets/Script/PublicStory/StoryPlayerControl.cs
+++ Assets/Script/PublicStory/StoryPlayerControl.cs
@@ -32,37 +32,37 @@ public class StoryPlayerControl : MonoBehaviour
     {
         Vector2 input = value.Get<Vector2>();
         //_storyMoveInput = new Vector3(-input.y, 0f, input.x);
-        //_storyMoveInput = new Vector3(input.x, 0f, input.y);
+        _storyMoveInput = new Vector3(input.x, 0f, input.y);
 
-        if (input.x == 0 && input.y > 0) // 仅按下 W 键
+        if (_storyMoveInput.x == 0 && _storyMoveInput.y > 0) // 仅按下 W 键
         {
             _storyMoveInput = new Vector3(-0.71f, 0f, 0.71f);
         }
-        else if (input.x == 0 && input.y < 0) // 仅按下 S 键
+        else if (_storyMoveInput.x == 0 && _storyMoveInput.y < 0) // 仅按下 S 键
         {
             _storyMoveInput = new Vector3(0.71f, 0f, -0.71f);
         }
-        else if (input.x < 0 && input.y == 0) // 仅按下 A 键
+        else if (_storyMoveInput.x < 0 && _storyMoveInput.y == 0) // 仅按下 A 键
         {
             _storyMoveInput = new Vector3(-0.71f, 0f, -0.71f);
         }
-        else if (input.x > 0 && input.y == 0) // 仅按下 D 键
+        else if (_storyMoveInput.x > 0 && _storyMoveInput.y == 0) // 仅按下 D 键
         {
             _storyMoveInput = new Vector3(0.71f, 0f, 0.71f);
         }
-        else if (input.x < 0 && input.y > 0) // 同时按下 W 和 A 键
+        else if (_storyMoveInput.x < 0 && _storyMoveInput.y > 0) // 同时按下 W 和 A 键
         {
             _storyMoveInput = new Vector3(-1f, 0f, 0f);
         }
-        else if (input.x > 0 && input.y > 0) // 同时按下 W 和 D 键
+        else if (_storyMoveInput.x > 0 && _storyMoveInput.y > 0) // 同时按下 W 和 D 键
         {
             _storyMoveInput = new Vector3(0f, 0f, 1f);
         }
-        else if (input.x < 0 && input.y < 0) // 同时按下 A 和 S 键
+        else if (_storyMoveInput.x < 0 && _storyMoveInput.y < 0) // 同时按下 A 和 S 键
         {
             _storyMoveInput = new Vector3(0f, 0f, -1f);
         }
-        else if (input.x > 0 && input.y < 0) // 同时按下 S 和 D 键
+        else if (_storyMoveInput.x > 0 && _storyMoveInput.y < 0) // 同时按下 S 和 D 键
         {
             _storyMoveInput = new Vector3(1f, 0f, 0f);
         }
