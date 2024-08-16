06869c67edd3d36227c4152e5c49b715890f98d0
GanXiKang <gan0612xk@hotmail.com>
2024-08-17 00:06:36 +0800

    feat :  編寫code

--- Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryDialogueControl_Girl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject player;
+
+    [Header("UITransform")]
+    public Transform[] target;
+    public static int _isAboveWho;
+
+    [Header("UIComponents")]
+    public GameObject dialogueUI;
+    public Text content;
+    public static bool isDialogue;
+
+    [Header("TextFile")]
+    public TextAsset[] textFile;
+    public int index;
+    public float textSpend;
+    public static int textCount = 1;
+    bool textFinish;
+
+    List<string> textList = new List<string>();
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        dialogueUI.SetActive(isDialogue);
     }
 }
