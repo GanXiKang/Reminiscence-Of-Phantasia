f153f938b098a9ac04481307d71c4ae30e946e47
GanXiKang <gan0612xk@hotmail.com>
2024-08-17 00:36:20 +0800

    feat ： 編寫code

--- Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
@@ -30,8 +30,54 @@ public class StoryDialogueControl_Girl : MonoBehaviour
         player = GameObject.Find("Player");
     }
 
+    void OnEnable()
+    {
+        GetTextFormFile(textFile[textCount]);
+        StartCoroutine(SetTextLabelIndexUI());
+    }
+
     void Update()
     {
         dialogueUI.SetActive(isDialogue);
+    }
+
+    void GetTextFormFile(TextAsset file)
+    {
+        textList.Clear();
+        index = 0;
+
+        var lineDate = file.text.Split("\n");
+
+        foreach (var line in lineDate)
+        {
+            textList.Add(line);
+        }
+    }
+
+    IEnumerator SetTextLabelIndexUI()
+    {
+        textFinish = false;
+        content.text = "";
+        switch (textList[index].Trim())
+        {
+            case "A":
+                index++;
+                break;
+
+            case "B":
+                index++;
+                break;
+
+            case "Œ¦Ô’½YÊø":
+                textFinish = true;
+                break;
+        }
+        for (int i = 0; i < textList[index].Length; i++)
+        {
+            content.text += textList[index][i];
+            yield return new WaitForSeconds(textSpend);
+        }
+        textFinish = true;
+        index++;
     }
 }
