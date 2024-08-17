0cc8737e33855dd5c78ad783717644ffef2d7b23
GanXiKang <gan0612xk@hotmail.com>
2024-08-17 23:58:37 +0800

    feat ： 優化

--- Assets/Script/House/CameraControl_House.cs
+++ Assets/Script/House/CameraControl_House.cs
@@ -6,27 +6,27 @@ public class CameraControl_House : MonoBehaviour
 {
     [Header("FreeLookCamera")]
     public GameObject freeLookCamera;
-    public static bool isFreeLook;
+    public static bool isFreeLook = false;
 
     [Header("CameraWorkbenchPosition")]
     public Transform workbenchPos;
-    public static bool isLookWorkbench;
+    public static bool isLookWorkbench = false;
 
     [Header("CameraDoorPosition")]
     public Transform doorPos;
-    public static bool isLookDoor;
+    public static bool isLookDoor = false;
 
     [Header("CameraBedPosition")]
     public Transform bedPos;
-    public static bool isLookBed;
+    public static bool isLookBed = false;
 
     [Header("CameraBookcasePosition")]
     public Transform bookcasePos;
-    public static bool isLookBookcase;
+    public static bool isLookBookcase = false;
 
     [Header("CameraShowcasePosition")]
     public Transform showcasePos;
-    public static bool isLookShowcase;
+    public static bool isLookShowcase = false;
 
     [Header("CameraMovement")]
     public Transform startPos;
@@ -34,8 +34,6 @@ public class CameraControl_House : MonoBehaviour
 
     void Start()
     {
-        isFreeLook = false;
-        isLookWorkbench = false;
         Invoke("StartFreeLookCamera", 1.5f);
     }
 
