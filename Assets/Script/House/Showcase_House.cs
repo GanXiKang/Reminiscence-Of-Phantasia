9412983360ca800bf7bcd9b3f0ec11c93b44acf4
GanXiKang <gan0612xk@hotmail.com>
2024-08-21 01:51:42 +0800

    feat ： 同下

--- Assets/Script/House/Showcase_House.cs
+++ Assets/Script/House/Showcase_House.cs
@@ -11,6 +11,24 @@ public class Showcase_House : MonoBehaviour
 
     void Update()
     {
-        
+        Leave();
+    }
+
+    void Leave()
+    {
+        if (CameraControl_House.isLookShowcase)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                StartCoroutine(LeaveShowcase());
+            }
+        }
+    }
+    IEnumerator LeaveShowcase()
+    {
+        LoadingUIControl_House.isOpenBlackScreen = true;
+        yield return new WaitForSeconds(1f);
+        CameraControl_House.isFreeLook = true;
+        CameraControl_House.isLookShowcase = false;
     }
 }
