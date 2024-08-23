af23acb489447187f4de4281d85506bc7769af95
GanXiKang <gan0612xk@hotmail.com>
2024-08-23 22:42:40 +0800

    feat ： 編寫code

--- Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
@@ -100,7 +100,8 @@ public class StoryDialogueControl_Girl : MonoBehaviour
         if (!isChoose) return;
 
         Vector3 chooseUITarget = chooseUI.position + new Vector3(350f, 0f, 0f);
-        chooseUI.position = Vector3.Lerp(chooseUI.position, chooseUITarget, 2f * Time.deltaTime);
+        chooseUI.position = Vector3.Lerp(chooseUI.position, chooseUITarget, 2f * Time.deltaTime); 
+        isChoose = false;
     }
 
     IEnumerator SetTextLabelIndexUI()
