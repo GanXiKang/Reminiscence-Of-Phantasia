5b948004f374ed72fd71f5b88b38a4c41ea212ca
GanXiKang <gan0612xk@hotmail.com>
2024-08-27 00:30:46 +0800

    feat ： 編寫code

---
+++ Assets/Script/Story_Girl/StoryStepSnow_Girl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryStepSnow_Girl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+        }
+    }
+}
