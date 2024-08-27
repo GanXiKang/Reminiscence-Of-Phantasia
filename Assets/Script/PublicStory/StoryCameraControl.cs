f13f0f65c8d6550d0a52f0e6f9a5d415b4ab0186
GanXiKang <gan0612xk@hotmail.com>
2024-08-27 22:11:48 +0800

    feat ： 優化code

--- Assets/Script/PublicStory/StoryCameraControl.cs
+++ Assets/Script/PublicStory/StoryCameraControl.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class StoryCameraControl : MonoBehaviour
 {
     [Header("CameraMovement")]
-    public Transform target;
     public float _smoothTime = 0.5f;
     public static bool isFollow;
     private Vector3 velocity = Vector3.zero;
+    Transform player;
 
     void Start()
     {
+        player = GameObject.Find("Player").transform;
         isFollow = true;
     }
 
@@ -19,7 +20,7 @@ public class StoryCameraControl : MonoBehaviour
     {
         if (isFollow)
         {
-            Vector3 _targetPosition = target.position;
+            Vector3 _targetPosition = player.position;
             transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref velocity, _smoothTime);
         }
     }
