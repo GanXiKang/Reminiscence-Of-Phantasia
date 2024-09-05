13c24047b7c66c631b3bff615f2dd486c56ac816
GanXiKang <gan0612xk@hotmail.com>
2024-09-05 22:44:26 +0800

    feat ： 編寫code

--- Assets/Script/House/EntrustControl_House.cs
+++ Assets/Script/House/EntrustControl_House.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EntrustControl_House : MonoBehaviour
 {
+    [Header("UI")]
+    public GameObject entrustUI;
+    public GameObject letterUI;
+    public GameObject contentUI;
+        
     void Start()
     {
         
