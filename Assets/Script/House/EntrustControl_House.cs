f7c01ca9197fff14e6c824bb88dbc2698c546804
GanXiKang <gan0612xk@hotmail.com>
2024-09-05 22:35:59 +0800

    feat : new script

---
+++ Assets/Script/House/EntrustControl_House.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrustControl_House : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
