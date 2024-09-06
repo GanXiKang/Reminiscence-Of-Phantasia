cab1573bab9582a21e81b996929694dfad192a6d
GanXiKang <gan0612xk@hotmail.com>
2024-09-07 01:48:14 +0800

    feat ： 編寫code

--- Assets/Script/House/BirdControl_House.cs
+++ Assets/Script/House/BirdControl_House.cs
@@ -7,7 +7,6 @@ public class BirdControl_House : MonoBehaviour
     Animator anim;
 
     public static bool isIdle = false;
-    public static bool isIdle_Out = false;
     public static bool isDeliver = false;
     public static bool isDeliver_Close = false;
     public static bool isHappy = false;
@@ -26,6 +25,47 @@ public class BirdControl_House : MonoBehaviour
 
     void Animation()
     {
-        
+        if (isIdle)
+        {
+            anim.SetBool("isIdle_In", true);
+            anim.SetBool("isIdle", true);
+            isIdle = false;
+        }
+        if (isDeliver)
+        {
+            anim.SetBool("isDeliver_Open", true);
+            anim.SetBool("isDeliver", true);
+            isDeliver = false;
+        }
+        if (isDeliver_Close)
+        {
+            anim.SetBool("isDeliver_Close", true);
+            anim.SetBool("isDeliver", false);
+            isDeliver = false;
+        }
+        if (isHappy)
+        {
+            anim.SetBool("isHappy", true);
+            Invoke("FalseAnimationisHappy", 0.2f);
+            isHappy = false;
+        }
+        if (isBye)
+        {
+            anim.SetBool("isBye", true);
+            anim.SetBool("isIdle_Out", true);
+            anim.SetBool("isIdle_In", false);
+            anim.SetBool("isIdle", false);
+            Invoke("FalseAnimationisBye", 1f);
+            isBye = false;
+        }
+    }
+
+    void FalseAnimationisHappy()
+    {
+        anim.SetBool("isHappy", false);
+    }
+    void FalseAnimationisBye()
+    {
+        anim.SetBool("isBye", false);
     }
 }
