e3f83ca9d253448a7972f80771d1443002a9acc9
GanXiKang <gan0612xk@hotmail.com>
2024-09-07 02:26:00 +0800

    feat ： rename

--- Assets/Script/House/EntrustControl_House.cs
+++ Assets/Script/House/EntrustControl_House.cs
@@ -7,30 +7,30 @@ public class EntrustControl_House : MonoBehaviour
 {
     [Header("UI")]
     public GameObject[] entrustUI;
-    public Button[] letterButton;
+    public Button[] deliverButton;
     public static bool isEntrustActive = false;
-    bool isLetterActive = false;
+    bool isDeliverActive = false;
     bool isContentActive = false;
 
     private void Start()
     {
-        LetterButtonInitialState();
+        DeliverButtonInitialState();
     }
     void Update()
     {
         entrustUI[0].SetActive(isEntrustActive);
-        entrustUI[1].SetActive(isLetterActive);
+        entrustUI[1].SetActive(isDeliverActive);
         entrustUI[2].SetActive(isContentActive);
 
         OpenUI();
     }
 
-    void LetterButtonInitialState()
+    void DeliverButtonInitialState()
     {
-        for (int i = 1; i < letterButton.Length; i++)
+        for (int i = 1; i < deliverButton.Length; i++)
         {
-            letterButton[i].GetComponent<CanvasGroup>().alpha = 0;
-            RectTransform rect = letterButton[i].GetComponent<RectTransform>();
+            deliverButton[i].GetComponent<CanvasGroup>().alpha = 0;
+            RectTransform rect = deliverButton[i].GetComponent<RectTransform>();
             rect.anchoredPosition = new Vector2(6f, rect.anchoredPosition.y);
         }
     }
@@ -44,28 +44,40 @@ public class EntrustControl_House : MonoBehaviour
             }
             else
             {
-                if (!isLetterActive && !isContentActive)
+                if (!isDeliverActive && !isContentActive)
                 {
-                    isLetterActive = true;
-                    StartCoroutine(AnimateButton(letterButton[1], 0f));
-                    StartCoroutine(AnimateButton(letterButton[2], 0.4f));
-                    StartCoroutine(AnimateButton(letterButton[3], 0.8f));
+                    isDeliverActive = true;
+                    StartCoroutine(AnimateButton(deliverButton[1], 0f));
+                    StartCoroutine(AnimateButton(deliverButton[2], 0.4f));
+                    StartCoroutine(AnimateButton(deliverButton[3], 0.8f));
                 }
             }
         }
         else
         {
             entrustUI[0].GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 1f);
-            isLetterActive = false;
+            isDeliverActive = false;
             isContentActive = false;
-            LetterButtonInitialState();
+            DeliverButtonInitialState();
         }
     }
 
-    public void Button_Letter(int _letter)
+    public void Button_Deliver(int _letter)
     {
         isContentActive = true;
-        isLetterActive = false;
+        isDeliverActive = false;
+    }
+    public void Button_Receive()
+    {
+
+    }
+    public void Button_Back()
+    {
+        
+    }
+    public void Button_Letter()
+    {
+        
     }
 
     IEnumerator AnimateButton(Button button, float delay)
