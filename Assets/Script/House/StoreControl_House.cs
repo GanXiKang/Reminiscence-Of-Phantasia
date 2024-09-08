5ee840ad229235971837c4d80061ae9e4dec8cc2
GanXiKang <gan0612xk@hotmail.com>
2024-09-09 01:45:41 +0800

    feat ： 編寫code

--- Assets/Script/House/StoreControl_House.cs
+++ Assets/Script/House/StoreControl_House.cs
@@ -7,6 +7,7 @@ public class StoreControl_House : MonoBehaviour
 {
     [Header("UI")]
     public GameObject[] storeUI;
+    public GameObject[] homePageButton;
     public static bool isStoreActive = false;
     bool isHomePageActive = false;
     bool isContentActive = false;
@@ -22,37 +23,40 @@ public class StoreControl_House : MonoBehaviour
 
     void OpenUI()
     {
-        if (isStoreActive)
+        if (!isStoreActive) return;
+
+        if (storeUI[0].GetComponent<RectTransform>().localScale.x < 1)
         {
-            if (storeUI[0].GetComponent<RectTransform>().localScale.x < 1)
-            {
-                storeUI[0].GetComponent<RectTransform>().localScale += new Vector3(2f, 2f, 0f) * Time.deltaTime;
-            }
-            else
-            {
-                if(!isHomePageActive && !isContentActive)
-                isHomePageActive = true;
-            }
+            storeUI[0].GetComponent<RectTransform>().localScale += new Vector3(2f, 2f, 0f) * Time.deltaTime;
         }
         else
         {
-            storeUI[0].GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 1f);
-            isHomePageActive = false;
-            isContentActive = false;
+            if (!isHomePageActive && !isContentActive)
+                isHomePageActive = true;
         }
     }
     void LeaveState()
     {
         UIAboveObject_House.isDialogBoxActive = false;
         DoorControl_House.isLeave = true;
+        isHomePageActive = false;
+        isContentActive = false;
         storeUI[1].GetComponent<CanvasGroup>().interactable = true;
+        storeUI[0].GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 1f);
     }
 
     public void Button_Product(int _product)
     {
-        isContentActive = true;
-        isHomePageActive = false;
-        CatControl_House.isBag = true;
+        if (isHomePageActive)
+        {
+            isContentActive = true;
+            isHomePageActive = false;
+            CatControl_House.isBag = true;
+        }
+        else
+        { 
+
+        }
     }
     public void Button_Buy()
     {
