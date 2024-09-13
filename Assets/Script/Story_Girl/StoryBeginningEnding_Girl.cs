abdb4205493febd8b91de3c471b44569030ea01d
GanXiKang <gan0612xk@hotmail.com>
2024-09-14 00:24:38 +0800

    feat : 編寫code

--- Assets/Script/Story_Girl/StoryBeginningEnding_Girl.cs
+++ Assets/Script/Story_Girl/StoryBeginningEnding_Girl.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryBeginningEnding_Girl : MonoBehaviour
 {
-    
+    [Header("UI")]
+    public GameObject storyUI;
+    public Text content;
+    public static bool isStory;
+    int _page = 0;
+
+    [Header("TextFile")]
+    public TextAsset textStart;
+    public TextAsset textEnding;
+    bool isStoryStart = true;
+
     void Start()
     {
-        
+        isStoryStart = true;
     }
 
     
     void Update()
     {
         
+    }
+
+    public void Button_Left() //上一頁
+    {
+        
+    }
+    public void Button_Right() //下一頁
+    {
+
     }
 }
