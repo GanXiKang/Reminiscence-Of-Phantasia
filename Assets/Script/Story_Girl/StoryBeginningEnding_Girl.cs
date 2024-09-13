c40e89497d5e183b54d4ba66e8dac9b806355e59
GanXiKang <gan0612xk@hotmail.com>
2024-09-14 00:38:43 +0800

    feat ： 編寫code

--- Assets/Script/Story_Girl/StoryBeginningEnding_Girl.cs
+++ Assets/Script/Story_Girl/StoryBeginningEnding_Girl.cs
@@ -9,30 +9,77 @@ public class StoryBeginningEnding_Girl : MonoBehaviour
     public GameObject storyUI;
     public Text content;
     public static bool isStory;
-    int _page = 0;
 
     [Header("TextFile")]
     public TextAsset textStart;
     public TextAsset textEnding;
-    bool isStoryStart = true;
+    public static bool isStoryEnding = false;
+    float _textSpend = 0.1f;
+    bool textFinish;
+    int _page;
+
+    List<string> textList = new List<string>();
 
     void Start()
     {
-        isStoryStart = true;
+        GetTextFormFile(textStart);
+        StartCoroutine(StorySystemUI());
     }
 
     
     void Update()
     {
-        
+        TextController();
+    }
+
+    void GetTextFormFile(TextAsset file)
+    {
+        textList.Clear();
+        _page = 0;
+
+        var lineDate = file.text.Split("\n");
+
+        foreach (var line in lineDate)
+        {
+            textList.Add(line);
+        }
+    }
+    void TextController()
+    {
+        if (!textFinish)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                _textSpend = 0f;
+            }
+        }
     }
 
     public void Button_Left() //上一頁
     {
-        
+        if (_page != 0)
+        {
+            _page--;
+            StartCoroutine(StorySystemUI());
+        }
     }
     public void Button_Right() //下一頁
     {
-
+        if (_page <= 3)
+        {
+            _page++;
+            StartCoroutine(StorySystemUI());
+        }
+    }
+    IEnumerator StorySystemUI()
+    {
+        textFinish = false;
+        content.text = "";
+        for (int i = 0; i < textList[_page].Length; i++)
+        {
+            content.text += textList[_page][i];
+            yield return new WaitForSeconds(_textSpend);
+        }
+        textFinish = true;
     }
 }
