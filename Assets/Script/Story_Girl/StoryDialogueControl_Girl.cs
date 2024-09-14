e03079a19722c61d4bcad52339edfe116b7d60fd
GanXiKang <gan0612xk@hotmail.com>
2024-09-14 22:52:37 +0800

    feat ： 爲主調整回來

--- Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryDialogueControl_Girl.cs
@@ -90,7 +90,7 @@ public class StoryDialogueControl_Girl : MonoBehaviour
     }
     void TextPosition()
     {
-        Vector3 offset = new Vector3(-300f, 300f, 0f);
+        Vector3 offset = new Vector3(-120f, 300f, 0f);
         if (isPlayerTalk)
         {
             Vector3 playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
