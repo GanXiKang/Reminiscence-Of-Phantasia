c1ca1b7a94c26df3e1795b61cb65fc68b1194f1a
GanXiKang <gan0612xk@hotmail.com>
2024-09-19 21:54:04 +0800

    feat ： 編寫code

--- Assets/Script/Story_Girl/StoryStepSnow_Girl.cs
+++ Assets/Script/Story_Girl/StoryStepSnow_Girl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class StoryStepSnow_Girl : MonoBehaviour
 {
+    public static bool isFirstStepSnow = true;
+
     void StepOnSnow()
     {
         StoryThermometerControl_Girl.isStepOnSnow = false;
@@ -13,6 +15,12 @@ public class StoryStepSnow_Girl : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            if (isFirstStepSnow)
+            {
+                StoryUIControl_Girl.isDialogue = true;
+                StoryDialogueControl_Girl._textCount = 9;
+                isFirstStepSnow = false;
+            }
             StoryThermometerControl_Girl.isStepOnSnow = true;
             Invoke("StepOnSnow", 2f);
         }
