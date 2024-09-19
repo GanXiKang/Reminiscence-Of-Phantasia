d0fa64964fe95b74905c3a863b5144fb891315c4
GanXiKang <gan0612xk@hotmail.com>
2024-09-20 00:37:12 +0800

    feat ： 編寫code

--- Assets/Script/Story_Girl/StoryColliderControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryColliderControl_Girl.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class StoryColliderControl_Girl : MonoBehaviour
 {
     public int _whatCollider;
-    bool isOnce;
+    bool isOnce = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            
+            if (_whatCollider == 1)
+            {
+                if (isOnce)
+                {
+                    isOnce = false;
+                    StoryUIControl_Girl.isDialogue = true;
+                    StoryDialogueControl_Girl._isAboveWho1 = 1;
+                    StoryDialogueControl_Girl._textCount = 30;
+                }
+            }
+            else 
+            {
+                StoryUIControl_Girl.isDialogue = true;
+                StoryDialogueControl_Girl._isAboveWho1 = 1;
+                StoryDialogueControl_Girl._textCount = 31;
+            }
         }
     }
 }
