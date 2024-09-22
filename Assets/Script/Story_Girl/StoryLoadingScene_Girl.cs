b57b043472b2e36806e34c2d6d8453e71403193d
GanXiKang <gan0612xk@hotmail.com>
2024-09-23 03:51:20 +0800

    feat ： 調整回正常版本

--- Assets/Script/Story_Girl/StoryLoadingScene_Girl.cs
+++ Assets/Script/Story_Girl/StoryLoadingScene_Girl.cs
@@ -108,10 +108,9 @@ public class StoryLoadingScene_Girl : MonoBehaviour
         {
             isLeftClose = false;
             isLoading = false;
-            //test
-            //StoryUIControl_Girl.isDialogue = true;
-            //StoryDialogueControl_Girl._isAboveWho1 = 1;
-            //StoryDialogueControl_Girl._textCount = 26;
+            StoryUIControl_Girl.isDialogue = true;
+            StoryDialogueControl_Girl._isAboveWho1 = 1;
+            StoryDialogueControl_Girl._textCount = 26;
         }
     }
     IEnumerator RightSwitchScene_Open()
