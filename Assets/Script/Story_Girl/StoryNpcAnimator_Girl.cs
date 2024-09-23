11099f08f9ce50b2e209f2a1f4f959055e8ca659
GanXiKang <gan0612xk@hotmail.com>
2024-09-24 01:46:25 +0800

    feat : 編寫code

--- Assets/Script/Story_Girl/StoryNpcAnimator_Girl.cs
+++ Assets/Script/Story_Girl/StoryNpcAnimator_Girl.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class StoryNpcAnimator_Girl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Npc")]
+    public int _who;
 
-    // Update is called once per frame
+    //01Irls
+    public static bool isHappy;
+    public static bool isSurprise;
+    public static bool isTrashCanLid;
+    public static bool isWearingRedHood;
+    //02GarbageCan
+
+    //03SantaClaus
+
+    //04Child_XiaoYan
+
+    //05Child_XiaoXin
+
+    //06Hunter
+
+    //09Camping
+
+    //11Wolf
+
     void Update()
     {
         
