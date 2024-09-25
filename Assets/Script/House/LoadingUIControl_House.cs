0d7f3a42c357b1b701a11ba0f3fe8e632bd7b99f
GanXiKang <gan0612xk@hotmail.com>
2024-09-25 22:24:29 +0800

    feat ： 調整

--- Assets/Script/House/LoadingUIControl_House.cs
+++ Assets/Script/House/LoadingUIControl_House.cs
@@ -13,20 +13,9 @@ public class LoadingUIControl_House : MonoBehaviour
     public static bool isCloseLoadingUI = false;
     public static bool isOpenLoadingUI = false;
 
-    //[Header("BlackScreen")]
-    //public GameObject blackScreen;
-    //public Image panel;
-    //Color currentColor;
-    //float _alpha = 0f;
-    //public float _screenSpeed = 50f;
-    //public static bool isCloseBlackScreen = false;
-    //public static bool isOpenBlackScreen = false;
-
     void Start()
     {
         isCloseLoadingUI = true;
-
-        //currentColor = panel.color;
     }
 
     void Update()
@@ -38,11 +27,6 @@ public class LoadingUIControl_House : MonoBehaviour
 
         CloseLoadingUI();
         OpenLoadingUI();
-
-        //currentColor.a = _alpha;
-        //panel.color = currentColor;
-        //OpenBlackScreen();
-        //CloseBlackScreen();
     }
 
     void CloseLoadingUI()
@@ -73,39 +57,4 @@ public class LoadingUIControl_House : MonoBehaviour
             }
         }
     }
-    //void CloseBlackScreen()
-    //{
-    //    if (isCloseBlackScreen)
-    //    {
-    //        if (_alpha > 0)
-    //        {
-    //            _alpha -= _screenSpeed * Time.deltaTime;
-    //        }
-    //        else
-    //        {
-    //            isCloseBlackScreen = false;
-    //            blackScreen.SetActive(false);
-    //        }
-    //    }
-    //}
-    //void OpenBlackScreen()
-    //{
-    //    if (isOpenBlackScreen)
-    //    {
-    //        blackScreen.SetActive(true);
-    //        if (_alpha < 1)
-    //        {
-    //            _alpha += _screenSpeed * Time.deltaTime;
-    //        }
-    //        else
-    //        {
-    //            isOpenBlackScreen = false;
-    //            Invoke("WaitCloseBlackSreen", 1f);
-    //        }
-    //    }
-    //}
-    //void WaitCloseBlackSreen()
-    //{
-    //    isCloseBlackScreen = true;
-    //}
 }
