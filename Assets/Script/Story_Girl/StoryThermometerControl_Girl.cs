64a3788a08b8592f6062728958d763490d3661fd
GanXiKang <gan0612xk@hotmail.com>
2024-09-26 00:17:19 +0800

    feat ： 調整code

--- Assets/Script/Story_Girl/StoryThermometerControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryThermometerControl_Girl.cs
@@ -47,52 +47,53 @@ public class StoryThermometerControl_Girl : MonoBehaviour
         Teach();
         SkillUI();
         UseMatch();
+        RenewTemperature();
         Limit();   
     }
 
     void Thermometer()
     {
         if (!isThermometer) return;
+        if (isDead) return;
 
         thermometerUI.SetActive(!StoryUIControl_Girl.isDialogue);
         Vector3 worldPos = player.transform.position + new Vector3(-8f, -3f, 0f);
         Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
         thermometerUI.transform.position = screenPos;
 
-        if (!StoryUIControl_Girl.isDialogue)
+        if (StoryUIControl_Girl.isDialogue) return;
+
+        if (!isUseMatches)
         {
-            if (!isUseMatches)
+            if (!isFireBeside)
             {
-                if (!isFireBeside)
+                if (!isStepOnSnow)
                 {
-                    if (!isStepOnSnow)
-                    {
-                        _temperature -= _decline * Time.deltaTime;
-                    }
-                    else
-                    {
-                        _temperature -= _snow * Time.deltaTime;
-                    }
+                    _temperature -= _decline * Time.deltaTime;
                 }
                 else
                 {
-                    _temperature += _rise * Time.deltaTime;
+                    _temperature -= _snow * Time.deltaTime;
                 }
             }
             else
             {
-                if (!isStepOnSnow)
-                {
-                    _temperature += _rise * Time.deltaTime;
-                }
-                else
-                {
-                    _temperature += _rise / 2 * Time.deltaTime;
-                }
+                _temperature += _rise * Time.deltaTime;
             }
-            energyBar.fillAmount = (_temperature - 35f) / 2;
-            temperature.text = _temperature.ToString("F1");
         }
+        else
+        {
+            if (!isStepOnSnow)
+            {
+                _temperature += _rise * Time.deltaTime;
+            }
+            else
+            {
+                _temperature += _rise / 2 * Time.deltaTime;
+            }
+        }
+        energyBar.fillAmount = (_temperature - 35f) / 2;
+        temperature.text = _temperature.ToString("F1");
     }
     void Teach()
     {
@@ -140,6 +141,13 @@ public class StoryThermometerControl_Girl : MonoBehaviour
     void FalseUseMatches()
     {
         isUseMatches = false;
+    }
+    void RenewTemperature()
+    {
+        if (!StoryGameControl_Girl.isRenewTemperature) return;
+
+        StoryGameControl_Girl.isRenewTemperature = false;
+        _temperature = 36f;
     }
     void Limit()
     {
