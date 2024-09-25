c7d08c00a6515d28537ddb2f7a5f340b03a78477
GanXiKang <gan0612xk@hotmail.com>
2024-09-26 04:06:54 +0800

    feat ： 調整

--- Assets/Script/Story_Girl/StoryInteractableControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryInteractableControl_Girl.cs
@@ -807,7 +807,7 @@ public class StoryInteractableControl_Girl : MonoBehaviour
             }
             else 
             {
-                StoryThermometerControl_Girl._matchQuantity = 40;
+                StoryThermometerControl_Girl._matchQuantity += 40;
                 StoryNpcAnimator_Girl._direction = 1;
                 StoryNpcAnimator_Girl.isGoGrandmom = true;
             }
