e6528d97b4110031a831d44c5a389da503c10d31
GanXiKang <gan0612xk@hotmail.com>
2024-09-27 00:56:21 +0800

    feat : 編寫code

--- Assets/Script/PublicAll/ColorLerpControl.cs
+++ Assets/Script/PublicAll/ColorLerpControl.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class ColorLerpControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Color DefaultColor = Color.white;
+    public Color HighlightColor = Color.blue;
+    public float _lerpSpeed = 1f;
+
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        DefaultColor = spriteRenderer.color;
+
+        StartCoroutine(UpdateColor());
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        StartCoroutine(UpdateColor());
+    }
+
+    IEnumerator UpdateColor()
+    {
+        Color lerpedColor = Color.white;
+        float currentTime = 0;
+
+        while (this.enabled)
+        {
+            lerpedColor = Color.Lerp(DefaultColor, HighlightColor, Mathf.PingPong(currentTime += (Time.deltaTime * _lerpSpeed / 1), 1));
+            spriteRenderer.color = lerpedColor;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        spriteRenderer.color = DefaultColor;
     }
 }
