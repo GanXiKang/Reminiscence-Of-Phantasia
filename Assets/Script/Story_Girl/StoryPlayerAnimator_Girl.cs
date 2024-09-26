15700a56330dded26d58d1a8f095377c4e36e579
GanXiKang <gan0612xk@hotmail.com>
2024-09-26 22:07:00 +0800

    feat : 編寫code

---
+++ Assets/Script/Story_Girl/StoryPlayerAnimator_Girl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryPlayerAnimator_Girl : MonoBehaviour
+{
+    Animator anim;
+    public static bool isCold;
+    public static bool isIronRod;
+    public static bool isMatch;
+    public static bool isStrongWind;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("isCold", isCold);
+        anim.SetBool("isIronRod", isIronRod);
+        anim.SetBool("isMatch", isMatch);
+        anim.SetBool("isStrongWind", isStrongWind);
+    }
+}
