1ac3914f0b816414a42cb2f9938827b56f047b45
GanXiKang <gan0612xk@hotmail.com>
2024-10-06 23:51:50 +0800

    feat : 添加if

--- Assets/Script/Story_Girl/StoryInteractableControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryInteractableControl_Girl.cs
@@ -663,12 +663,15 @@ public class StoryInteractableControl_Girl : MonoBehaviour
                     break;
 
                 case 11:
-                    BGM.PlayOneShot(fight);
-                    StoryNpcAnimator_Girl.isScared = true;
-                    StoryNpcAnimator_Girl.isFightRunAway = true;
-                    StoryUIControl_Girl.isDialogue = true;
-                    StoryDialogueControl_Girl._isAboveWho1 = 1;
-                    StoryDialogueControl_Girl._textCount = 51;
+                    if (isTrashCanLid)
+                    {
+                        BGM.PlayOneShot(fight);
+                        StoryNpcAnimator_Girl.isScared = true;
+                        StoryNpcAnimator_Girl.isFightRunAway = true;
+                        StoryUIControl_Girl.isDialogue = true;
+                        StoryDialogueControl_Girl._isAboveWho1 = 1;
+                        StoryDialogueControl_Girl._textCount = 51;
+                    }
                     break;
             }
         }
