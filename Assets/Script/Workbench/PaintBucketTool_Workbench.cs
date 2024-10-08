71c54654a33062e6a2f467dc869b6ba42b86b1a9
GanXiKang <gan0612xk@hotmail.com>
2024-10-08 23:37:28 +0800

    feat ： 調整

--- Assets/Script/Workbench/PaintBucketTool_Workbench.cs
+++ Assets/Script/Workbench/PaintBucketTool_Workbench.cs
@@ -49,14 +49,14 @@ public class PaintBucketTool_Workbench : MonoBehaviour
                     Color replacementColor = colorPicker.selectedColor;
                     WorkbenchControl_House.isChangeColor[_number] = true;
 
-                    //FloodFill(texture, x, y, targetColor, replacementColor);
+                    FloodFill(texture, x, y, targetColor, replacementColor);
 
-                    if (progressBar != null)
-                    {
-                        progressBar.gameObject.SetActive(true);
-                    }
+                    //if (progressBar != null)
+                    //{
+                    //    progressBar.gameObject.SetActive(true);
+                    //}
 
-                    StartCoroutine(FloodFillWithProgress(texture, x, y, targetColor, replacementColor));
+                    //StartCoroutine(FloodFillWithProgress(texture, x, y, targetColor, replacementColor));
                 }
             }
         }
