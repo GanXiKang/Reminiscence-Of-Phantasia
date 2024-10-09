24a61259c2dcb6a97136514dc2179d193711fec6
GanXiKang <gan0612xk@hotmail.com>
2024-10-09 23:42:28 +0800

    feat ： 處理解釋

--- Assets/Script/Workbench/CustomCursorControl_Workbench.cs
+++ Assets/Script/Workbench/CustomCursorControl_Workbench.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class CustomCursorControl_Workbench : MonoBehaviour
 {
-    public Texture2D customCursorTexture;  // 自定義光標圖像
-    public Vector2 hotSpot = Vector2.zero; // 自定義光標的熱點
-    private bool isCursorChanged = false;  // 標記是否已更換光標
+    public Texture2D customCursorTexture; 
+    public Vector2 hotSpot = Vector2.zero; 
+    private bool isCursorChanged = false;  
 
     public GameObject objectToMove;
 
     void Update()
     {
-        // 取得螢幕寬度的一半
         float screenHalfWidth = Screen.width / 2;
 
-        // 判斷滑鼠是否在右半邊
         if (Input.mousePosition.x > screenHalfWidth)
         {
-            // 如果滑鼠位於右半邊且光標未被更換，則更換為自定義光標
             if (!isCursorChanged)
             {
                 Cursor.SetCursor(customCursorTexture, hotSpot, CursorMode.Auto);
@@ -29,7 +26,6 @@ public class CustomCursorControl_Workbench : MonoBehaviour
         }
         else
         {
-            // 如果滑鼠位於左半邊且光標已被更換，則還原為默認光標
             if (isCursorChanged)
             {
                 Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
@@ -42,11 +38,9 @@ public class CustomCursorControl_Workbench : MonoBehaviour
     {
         Vector3 mousePosition = Input.mousePosition;
 
-        // 將滑鼠位置轉換為世界座標，並指定Z軸距離
-        mousePosition.z = Camera.main.nearClipPlane + 5f;  // 這裡設定Z軸距離，根據場景需要調整
+        mousePosition.z = Camera.main.nearClipPlane + 5f;
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        // 將3D物件移動到滑鼠的世界座標位置
         objectToMove.transform.position = worldPosition;
     }
 }
