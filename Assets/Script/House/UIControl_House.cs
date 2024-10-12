d1fe19ab8a308a5d2335164066a4703155f8a1fb
GanXiKang <gan0612xk@hotmail.com>
2024-10-13 02:04:34 +0800

    feat : move code

--- Assets/Script/House/UIControl_House.cs
+++ Assets/Script/House/UIControl_House.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("LoadingUI")]
+    public GameObject loadingUI;
+    public Image a, b, c;
+    float value = 1;
+    public float _loadingSpeed = 1.5f;
+    public static bool isCloseLoadingUI = false;
+    public static bool isOpenLoadingUI = false;
+
     void Start()
     {
-        
+        isCloseLoadingUI = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LoadingUI();
+        CloseLoadingUI();
+        OpenLoadingUI();
+    }
+
+    void LoadingUI()
+    {
+        loadingUI.SetActive(isCloseLoadingUI || isOpenLoadingUI);
+        a.fillAmount = value;
+        b.fillAmount = value;
+        c.fillAmount = value;
+    }
+    void CloseLoadingUI()
+    {
+        if (isCloseLoadingUI)
+        {
+            if (value > 0)
+            {
+                value -= _loadingSpeed * Time.deltaTime;
+            }
+            else
+            {
+                isCloseLoadingUI = false;
+            }
+        }
+    }
+    void OpenLoadingUI()
+    {
+        if (isOpenLoadingUI)
+        {
+            if (value < 1)
+            {
+                value += _loadingSpeed * Time.deltaTime;
+            }
+            else
+            {
+                isOpenLoadingUI = false;
+            }
+        }
     }
 }
