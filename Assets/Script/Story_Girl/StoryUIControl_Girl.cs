52fc1c49516198552c01c9abc935551d2b772381
GanXiKang <gan0612xk@hotmail.com>
2024-10-14 01:40:11 +0800

    feat ： 結束

--- Assets/Script/Story_Girl/StoryUIControl_Girl.cs
+++ Assets/Script/Story_Girl/StoryUIControl_Girl.cs
@@ -10,8 +10,8 @@ public class StoryUIControl_Girl : MonoBehaviour
 
     [Header("StoryUI")]
     public GameObject storyUI;
-    public static bool isStoryStart = false;
-    public static bool isStoryEnding = true;
+    public static bool isStoryStart = true;
+    public static bool isStoryEnding = false;
 
     [Header("TransitionUI")]
     public GameObject transitionUI;
