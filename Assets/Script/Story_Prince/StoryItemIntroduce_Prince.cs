abce8532df43aa82c04af3abb1728050c751ad7e
GanXiKang <gan0612xk@hotmail.com>
2024-10-14 17:06:12 +0800

    feat ： copy code

--- Assets/Script/Story_Prince/StoryItemIntroduce_Prince.cs
+++ Assets/Script/Story_Prince/StoryItemIntroduce_Prince.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryItemIntroduce_Prince : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("IntroduceUI")]
+    public GameObject introduce;
+    public GameObject panel;
+    public Text content;
+    string[] introduceItem = new string[10];
+    public static bool isIntroduce = true;
+
+    [Header("IntroduceTransform")]
+    public Transform[] buttonTransform;
+
     void Start()
     {
-        
+        ItemIntroduceContent();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointEnter(int _whichItem)
     {
-        
+        panel.GetComponent<RectTransform>().position = buttonTransform[_whichItem].position + new Vector3(-80f, 150f, 0f);
+        content.text = introduceItem[StoryBagControl._gridsItemNumber[_whichItem]].ToString();
+        if (isIntroduce)
+        {
+            introduce.SetActive(true);
+        }
+        else
+        {
+            introduce.SetActive(false);
+        }
+    }
+    public void OnPointExit()
+    {
+        introduce.SetActive(false);
+    }
+
+    void ItemIntroduceContent()
+    {
+        introduceItem[1] = "\n";
+        introduceItem[2] = "\n";
+        introduceItem[3] = "\n";
+        introduceItem[4] = "\n";
+        introduceItem[5] = "\n";
+        introduceItem[6] = "\n";
+        introduceItem[7] = "\n";
+        introduceItem[8] = "\n";
+        introduceItem[9] = "\n";
     }
 }
