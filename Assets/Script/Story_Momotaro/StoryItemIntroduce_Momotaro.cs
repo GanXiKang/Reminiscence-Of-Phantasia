fe1927b3f15a76a7384ca27c9cadf471c03d817a
GanXiKang <gan0612xk@hotmail.com>
2024-10-14 17:10:07 +0800

    feat ： 添加内容

--- Assets/Script/Story_Momotaro/StoryItemIntroduce_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryItemIntroduce_Momotaro.cs
@@ -40,14 +40,16 @@ public class StoryItemIntroduce_Momotaro : MonoBehaviour
 
     void ItemIntroduceContent()
     {
-        introduceItem[1] = "\n";
-        introduceItem[2] = "\n";
-        introduceItem[3] = "\n";
-        introduceItem[4] = "\n";
-        introduceItem[5] = "\n";
-        introduceItem[6] = "\n";
-        introduceItem[7] = "\n";
-        introduceItem[8] = "\n";
-        introduceItem[9] = "\n";
+        introduceItem[1] = "糯米糰子\n據說吃下後就能聽從命令";
+        introduceItem[2] = "金糯米糰子\n給動物吃下就能變身成牠";
+        introduceItem[3] = "銀糯米糰子\n吃下後就能變身";
+        introduceItem[4] = "單支鼓棒\n這樣沒辦法使用";
+        introduceItem[5] = "金箍棒\n能夠伸縮自如的超強武器";
+        introduceItem[6] = "銀鼓棒\n很漂亮但依舊只有一支";
+        introduceItem[7] = "銀鈴鐺\n鈴聲非常清脆";
+        introduceItem[8] = "木製鈴鐺\n感覺小動物會喜歡";
+        introduceItem[9] = "笛子\n有些老舊了";
+        introduceItem[10] = "銀笛\n可以吹出優美的旋律";
+        introduceItem[11] = "金笛\n可以吹出華麗的笛聲";
     }
 }
