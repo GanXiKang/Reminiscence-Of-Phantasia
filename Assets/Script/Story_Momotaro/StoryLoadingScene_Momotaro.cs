03d43c742452cba11a87610be94d1f1d5baaace5
GanXiKang <gan0612xk@hotmail.com>
2024-10-14 17:35:44 +0800

    feat ： new

---
+++ Assets/Script/Story_Momotaro/StoryLoadingScene_Momotaro.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryLoadingScene_Momotaro : MonoBehaviour
+{
+    [Header("Musia")]
+    public AudioSource BGM;
+    public AudioClip riverSideBGM, forestBGM, mountainBGM, plazaBGM;
+    public AudioClip switchScene;
+    bool isPlayOnce = true;
+
+    [Header("Scene")]
+    public GameObject riverSide;
+    public GameObject forest;
+    public GameObject mountain;
+    public GameObject plaza;
+
+    [Header("LoadingUI")]
+    public GameObject loadingUI;
+    public Image a, b, c;
+    public static bool isLoading = false;
+    public static bool isLeftOpen = false;     //ªÿ»•
+    public static bool isLeftClose = false;
+    public static bool isRightOpen = false;    //«∞ﬂM
+    public static bool isRightClose = false;
+    float _loadingSpeed = 1.5f;
+    bool isOnce = true;
+
+    void Update()
+    {
+        loadingUI.SetActive(isLoading);
+
+        SwitchSceneMusia();
+        LeftRightSwitch();
+    }
+
+    void SwitchSceneMusia()
+    {
+        if (!isLeftOpen && !isRightOpen) return;
+        if (!isPlayOnce) return;
+
+        BGM.PlayOneShot(switchScene);
+        isPlayOnce = false;
+    }
+    void LeftRightSwitch()
+    {
+        if (isLeftOpen)
+        {
+            StartCoroutine(LeftSwitchScene_Open());
+        }
+        if (isLeftClose)
+        {
+            StartCoroutine(LeftSwitchScene_Close());
+        }
+        if (isRightOpen)
+        {
+            StartCoroutine(RightSwitchScene_Open());
+        }
+        if (isRightClose)
+        {
+            StartCoroutine(RightSwitchScene_Close());
+        }
+    }
+    void BarValue(Image bar, bool isAdd)
+    {
+        if (isAdd)
+        {
+            if (bar.fillAmount < 1)
+            {
+                bar.fillAmount += _loadingSpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (bar.fillAmount > 0)
+            {
+                bar.fillAmount -= _loadingSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator LeftSwitchScene_Open()
+    {
+        isLoading = true;
+        a.fillOrigin = 0;
+        b.fillOrigin = 0;
+        c.fillOrigin = 0;
+
+        BarValue(a, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(c, true);
+
+        if (c.fillAmount == 1)
+        {
+            isLeftOpen = false;
+            yield return new WaitForSeconds(0.5f);
+            //street.SetActive(true);
+            //forest.SetActive(false);
+            //BGM.Stop();
+            //BGM.clip = streetBGM;
+            //BGM.Play();
+            isRightClose = true;
+        }
+    }
+    IEnumerator LeftSwitchScene_Close()
+    {
+        a.fillOrigin = 0;
+        b.fillOrigin = 0;
+        c.fillOrigin = 0;
+
+        BarValue(c, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(a, false);
+
+        if (a.fillAmount == 0)
+        {
+            isPlayOnce = true;
+            isLeftClose = false;
+            isLoading = false;
+            if (isOnce)
+            {
+                isOnce = false;
+                StoryNpcAnimator_Girl._direction = 0;
+                StoryUIControl_Girl.isDialogue = true;
+                StoryDialogueControl_Girl._isAboveWho1 = 1;
+                StoryDialogueControl_Girl._textCount = 26;
+            }
+        }
+    }
+    IEnumerator RightSwitchScene_Open()
+    {
+        isLoading = true;
+        a.fillOrigin = 1;
+        b.fillOrigin = 1;
+        c.fillOrigin = 1;
+
+        BarValue(a, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, true);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(c, true);
+
+        if (c.fillAmount == 1)
+        {
+            isRightOpen = false;
+            yield return new WaitForSeconds(0.5f);
+            //forest.SetActive(true);
+            //street.SetActive(false);
+            //BGM.Stop();
+            //BGM.clip = forestBGM;
+            //BGM.Play();
+            isLeftClose = true;
+        }
+    }
+    IEnumerator RightSwitchScene_Close()
+    {
+        a.fillOrigin = 1;
+        b.fillOrigin = 1;
+        c.fillOrigin = 1;
+
+        BarValue(c, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(b, false);
+        yield return new WaitForSeconds(0.2f);
+        BarValue(a, false);
+
+        if (a.fillAmount == 0)
+        {
+            isPlayOnce = true;
+            isRightClose = false;
+            isLoading = false;
+        }
+    }
+}
---
+++ Assets/Script/Story_Prince/StoryLoadingScene_Prince.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryLoadingScene_Prince : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
