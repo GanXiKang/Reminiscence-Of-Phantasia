42d8ff3175f626cd70e6c3dc9132764c6e16f82f
GanXiKang <gan0612xk@hotmail.com>
2024-10-15 09:24:28 +0800

    feat ： 調整時間

--- Assets/Script/House/WorkbenchControl_House.cs
+++ Assets/Script/House/WorkbenchControl_House.cs
@@ -472,7 +472,7 @@ public class WorkbenchControl_House : MonoBehaviour
             _process = 0;
             isFinishStoryBook = false;
             testStoryBook.SetBool("isOpenTest", false);
-            Invoke("PaperClosebyBook", 1.8f);
+            Invoke("PaperClosebyBook", 1.2f);
         }
         if (!isPaperAdjustScale)
         {
