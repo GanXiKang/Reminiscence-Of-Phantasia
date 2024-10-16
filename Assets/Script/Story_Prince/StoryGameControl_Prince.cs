88e1759dcf615f39cc5cfefc8719447ee7ef2bce
GanXiKang <gan0612xk@hotmail.com>
2024-10-17 00:15:32 +0800

    feat ： copy

--- Assets/Script/Story_Prince/StoryGameControl_Prince.cs
+++ Assets/Script/Story_Prince/StoryGameControl_Prince.cs
@@ -6,59 +6,13 @@ public class StoryGameControl_Prince : MonoBehaviour
 {
     GameObject player;
 
-    [Header("TeachWall")]
-    public GameObject teachWall;
-    public static bool isWallActive = true;
-
-    [Header("GarbageCan")]
-    public GameObject trashcanLid;
-    public static bool isTrashcanLidActice = true;
-
-    [Header("Resurrection")]
-    public Transform streetPoint;
-    public Transform forestPoint;
-    public static bool isInStreet = true;
-    public static bool isResurrection = false;
-    public static bool isRenewTemperature = false;
-    StoryPlayerControl playerControl;
-
     void Start()
     {
         player = GameObject.Find("Player");
-        playerControl = player.GetComponent<StoryPlayerControl>();
     }
 
     void Update()
     {
-        teachWall.SetActive(isWallActive);
-        trashcanLid.SetActive(isTrashcanLidActice);
-
-        PlayerResurrection();
-    }
-
-    void PlayerResurrection()
-    {
-        if (!isResurrection) return;
-
-        isResurrection = false;
-        StartCoroutine(ResurrectionState());
-    }
-    IEnumerator ResurrectionState()
-    {
-        playerControl.enabled = false;
-        yield return new WaitForSeconds(0.7f);
-        if (isInStreet)
-        {
-            player.transform.position = streetPoint.position;
-        }
-        else
-        {
-            player.transform.position = forestPoint.position;
-        }
-        isRenewTemperature = true;
-        StoryThermometerControl_Girl.isDead = false;
-        StoryThermometerControl_Girl._matchQuantity = 20;
-        yield return new WaitForSeconds(0.5f);
-        playerControl.enabled = true;
+        
     }
 }
