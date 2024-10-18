9050a472df67a25489743196eaf889aea6b85960
GanXiKang <gan0612xk@hotmail.com>
2024-10-19 02:49:33 +0800

    feat ： 添加判斷

--- Assets/Script/PublicStory/StoryPlayerControl.cs
+++ Assets/Script/PublicStory/StoryPlayerControl.cs
@@ -147,6 +147,7 @@ public class StoryPlayerControl : MonoBehaviour
                !StoryLoadingScene_Girl.isLoading &&
                !StoryUIControl_Girl.isDialogue &&
 
+               !StoryRiceDumpling_Momotaro.isEat &&
                !StoryPlayerAnimator_Momotaro.isRaccoonStone &&
                !StoryUIControl_Momotaro.isPerformances;
     }
