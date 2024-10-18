a849cab0b945b60e639b45f7a79c84ffab9d427a
GanXiKang <gan0612xk@hotmail.com>
2024-10-18 08:06:40 +0800

    feat ： 編寫code

--- Assets/Script/Story_Momotaro/StoryRiceDumpling_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryRiceDumpling_Momotaro.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryRiceDumpling_Momotaro : MonoBehaviour
 {
+    GameObject player;
+
+    [Header("Musia")]
+    public AudioSource BGM;
+    public AudioClip use;
+
+    [Header("SkillUI")]
+    public GameObject skill;
+    public static bool isSkillActive;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
     void Update()
     {
-        
+        skill.SetActive(isSkillActive);
+    }
+
+    public void RiceDumpling_Button()
+    {
+
     }
 }
