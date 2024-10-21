eb03d86c0056d874127e8e628eead4499f5e9ca8
GanXiKang <gan0612xk@hotmail.com>
2024-10-21 09:34:59 +0800

    feat ： 編寫code

--- Assets/Script/Story_Momotaro/StoryStrongWind_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryStrongWind_Momotaro.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class StoryStrongWind_Momotaro : MonoBehaviour
 {
+    [Header("Wind")]
+    public GameObject wind;
+    public float windDuration = 3f; // 大风持续时间
+    public float windCooldown = 5f; // 大风间隔时间
+    bool isWindActive = false;
+
+    [Header("BlownAway")]
+    public Transform originalPoint;
     public static bool isBlownAway = false;
 
     void Update()
