98f6ddbcec7f8a3382911f8ef741b2a090a63aaf
GanXiKang <gan0612xk@hotmail.com>
2024-10-21 09:17:37 +0800

    feat : 編寫code

--- Assets/Script/Story_Momotaro/StoryStrongWind_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryStrongWind_Momotaro.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class StoryStrongWind_Momotaro : MonoBehaviour
 {
-    public float pushSpeed = 5f; // 玩家被推开速度
-    private Transform playerTransform;
-    private Vector3 pushDirection;
-
     public static bool isBlownAway = false;
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        if (isBlownAway && playerTransform != null)
-        {
-            // 持续推动玩家离开物体
-            playerTransform.position = Vector3.MoveTowards(playerTransform.position, playerTransform.position + pushDirection, pushSpeed * Time.deltaTime);
-        }
+       
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            isBlownAway = true;
-            playerTransform = other.transform;
-
-            // 计算玩家相对于触发器的推开方向
-            pushDirection = (playerTransform.position - transform.position).normalized;
-        }
+        print("yes");
     }
     private void OnTriggerExit(Collider other)
     {
