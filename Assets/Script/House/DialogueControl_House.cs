2c26c07900e7d48e69af95113fc232a8fa9d0580
GanXiKang <gan0612xk@hotmail.com>
2024-10-24 03:29:42 +0800

    feat : debug //code

--- Assets/Script/House/DialogueControl_House.cs
+++ Assets/Script/House/DialogueControl_House.cs
@@ -73,7 +73,7 @@ public class DialogueControl_House : MonoBehaviour
         switch (textList[_index].Trim())
         {
             case "Player":
-                StartCoroutine(UIMoveToPosition(whoDialogue[1], dialoguePos[1].anchoredPosition));
+                //StartCoroutine(UIMoveToPosition(whoDialogue[1], dialoguePos[1].anchoredPosition));
                 _whoDialogue = 1;
                 _index++;
                 break;
