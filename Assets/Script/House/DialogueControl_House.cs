6a93c564dc4926471f74e32e2a153b726ea747cb
GanXiKang <gan0612xk@hotmail.com>
2024-10-24 01:45:52 +0800

    feat ： 編寫code

--- Assets/Script/House/DialogueControl_House.cs
+++ Assets/Script/House/DialogueControl_House.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class DialogueControl_House : MonoBehaviour
 {
+    [Header("UI")]
+    public GameObject dialogueUI;
+    public GameObject[] whoDialogue;
     public static bool isDialogue = false;
+
+    [Header("Position")]
+    public Transform[] dialoguePos;
 
     void Start()
     {
