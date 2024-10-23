1431e69bff784097ead90edf0654e6f2f7ac6192
GanXiKang <gan0612xk@hotmail.com>
2024-10-24 01:29:06 +0800

    feat ： 編寫code

--- Assets/Script/House/AvatarControl_House.cs
+++ Assets/Script/House/AvatarControl_House.cs
@@ -1,20 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AvatarControl_House : MonoBehaviour
 {
+    Image avatar;
+
     [Header("Sprite")]
     public Sprite normal;
     public Sprite mouth;
     public int _who;
     public static int _whoDialogue;
+    bool isSwitching = false;
+    bool isSwitchActive = false;
+
+    void Start()
+    {
+        avatar = GetComponent<Image>();
+    }
 
     void Update()
     {
-        if (_who == _whoDialogue) return;
-
-        
+        if (_who == _whoDialogue && isSwitchActive && !isSwitching)
+        {
+            StartCoroutine(SwitchAvatar());
+        }
     }
 
+    IEnumerator SwitchAvatar()
+    {
+        isSwitching = true;
+
+        while (isSwitchActive)
+        {
+            avatar.sprite = mouth;
+            yield return new WaitForSeconds(0.5f); // 切換到mouth並等待0.5秒
+
+            avatar.sprite = normal;
+            yield return new WaitForSeconds(0.5f); // 切換到normal並等待0.5秒
+        }
+
+        isSwitching = false;
+    }
 }
