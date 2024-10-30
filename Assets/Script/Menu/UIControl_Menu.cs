e6ab39a55e1919ecc8861deb16dbbae091de12b1
GanXiKang <gan0612xk@hotmail.com>
2024-10-31 03:09:13 +0800

    feat ： 編寫code

--- Assets/Script/Menu/UIControl_Menu.cs
+++ Assets/Script/Menu/UIControl_Menu.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class UIControl_Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Button_Quit()
+    {
+        Application.Quit();
     }
 }
