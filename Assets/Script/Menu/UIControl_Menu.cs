a88472b69c2a9f23757e79d276af6f1dc0d58c72
GanXiKang <gan0612xk@hotmail.com>
2024-10-31 03:16:52 +0800

    feat ： 編寫code

--- Assets/Script/Menu/UIControl_Menu.cs
+++ Assets/Script/Menu/UIControl_Menu.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIControl_Menu : MonoBehaviour
 {
+    [Header("Musia")]
+    public AudioSource BGM;
+    public AudioClip onClick;
+
+    [Header("UI")]
+    public GameObject[] menuUI;
+    public Slider sliderBGM;
+    public Toggle fullScreen;
+
+    public static float volumeBGM = 0.7f;
+    public static bool isFullS;
+
     void Start()
     {
         
@@ -14,8 +28,30 @@ public class UIControl_Menu : MonoBehaviour
         
     }
 
+    public void Button_Start()
+    {
+        
+    }
+    public void Button_Continue()
+    {
+
+    }
+    public void Button_Setting()
+    {
+
+    }
     public void Button_Quit()
     {
         Application.Quit();
+    }
+    public void Volume_BGM()
+    {
+        volumeBGM = sliderBGM.value;
+        BGM.volume = volumeBGM;
+    }
+    public void FullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+        isFullS = isFullScreen;
     }
 }
