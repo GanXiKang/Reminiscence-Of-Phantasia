7c5ae10c305bbec1f940705756b24ee480ea8cab
GanXiKang <gan0612xk@hotmail.com>
2024-11-01 01:45:51 +0800

    feat  編寫

--- Assets/Script/House/Showcase_House.cs
+++ Assets/Script/House/Showcase_House.cs
@@ -9,6 +9,7 @@ public class Showcase_House : MonoBehaviour
     public Transform showPoint;
     Transform originalPoint;
     int _showNum = 0;
+    bool isShow = false;
 
     void Start()
     {
@@ -18,6 +19,27 @@ public class Showcase_House : MonoBehaviour
     void Update()
     {
         Leave();
+    }
+
+    void StoryBookShow()
+    {
+        switch (_showNum)
+        {
+            case 0:
+                originalPoint = storyBook[0].transform;
+                storyBook[0].transform.position = showPoint.position;
+                storyBook[0].transform.rotation = showPoint.rotation;
+                break;
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+        }
     }
 
     void Leave()
