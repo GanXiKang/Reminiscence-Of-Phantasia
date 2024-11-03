331c308b067c4f327c918115bbfd20f9b8fb03b9
GanXiKang <gan0612xk@hotmail.com>
2024-11-04 02:10:43 +0800

    feat ： 調整時間計算

--- Assets/Script/Story_Prince/StorySkillControl_Prince.cs
+++ Assets/Script/Story_Prince/StorySkillControl_Prince.cs
@@ -56,7 +56,7 @@ public class StorySkillControl_Prince : MonoBehaviour
 
         if (isRotating)
         {
-            float _rotationSpeed = Mathf.Lerp(30f, 90f, 2);
+            float _rotationSpeed = Mathf.Lerp(30f, 90f, Time.deltaTime);
             pointer.transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
             print(_rotationSpeed);
             _energyValue -= _rotation * Time.deltaTime;
