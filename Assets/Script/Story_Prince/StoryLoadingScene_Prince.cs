2b10c98d1f3ec65b477194aad498d4e8f8306c33
GanXiKang <gan0612xk@hotmail.com>
2024-11-06 01:16:15 +0800

    feat : 編寫code

--- Assets/Script/Story_Prince/StoryLoadingScene_Prince.cs
+++ Assets/Script/Story_Prince/StoryLoadingScene_Prince.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryLoadingScene_Prince : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Musia")]
+    public AudioSource BGM;
+    public AudioClip nowBGM, pastBGM, futureBGM;
+
+    [Header("Scene")]
+    public GameObject now;
+    public GameObject past;
+    public GameObject future;
+    public static bool isNowScene = true;
+    public static bool isPastScene = false;
+    public static bool isFutureScene = false;
+    public static bool isChange = false;
+
+    [Header("LoadingUI")]
+    public GameObject loadingUI;
+    public Image a;
+
     void Start()
     {
         
