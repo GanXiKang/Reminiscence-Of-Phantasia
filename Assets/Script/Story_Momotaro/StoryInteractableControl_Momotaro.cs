f4aa7c11cfe621f6978e6fe06c60a014c4442bab
GanXiKang <gan0612xk@hotmail.com>
2024-11-17 16:59:48 +0800

    feat ： 添加

--- Assets/Script/Story_Momotaro/StoryInteractableControl_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryInteractableControl_Momotaro.cs
@@ -65,6 +65,7 @@ public class StoryInteractableControl_Momotaro : MonoBehaviour
 
     //02Goddess
     int _itemGoddess;
+    public static bool isAnswerLie = false;
     //06Raccoon
     public static bool isFinishWork = false;
     //09Parrot
