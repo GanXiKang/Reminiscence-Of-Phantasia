793ce3c33481d0308ba5b7bfdf8b0c75699c05a6
GanXiKang <gan0612xk@hotmail.com>
2024-11-19 01:39:42 +0800

    feat ： 測試

--- Assets/Script/House/Main/UIAboveObject_House.cs
+++ Assets/Script/House/Main/UIAboveObject_House.cs
@@ -40,7 +40,7 @@ public class UIAboveObject_House : MonoBehaviour
     {
         player = GameObject.Find("Player");
 
-        isAboveShowcase = true;
+        isAboveBookcase = true;
     }
 
     void Update()
