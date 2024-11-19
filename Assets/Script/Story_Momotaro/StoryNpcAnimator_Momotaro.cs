3d7f328cf0ea932bf67569a262f4694a76c06070
GanXiKang <gan0612xk@hotmail.com>
2024-11-20 04:35:04 +0800

    feat ： 編寫code

--- Assets/Script/Story_Momotaro/StoryNpcAnimator_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryNpcAnimator_Momotaro.cs
@@ -20,6 +20,7 @@ public class StoryNpcAnimator_Momotaro : MonoBehaviour
     //03Donkey
     public static bool isShy = false;
     public static bool isGift = false;
+    public static bool isWrongGift = false;
     //04Monkey
     public static bool isWalk_Monkey = false;
     public static bool isWalkGold_Monkey = false;
@@ -90,6 +91,7 @@ public class StoryNpcAnimator_Momotaro : MonoBehaviour
                 anim.SetInteger("Dating", _dating);
                 anim.SetBool("isShy", isShy);
                 anim.SetBool("isGift", isGift);
+                anim.SetBool("isWrongGift", isWrongGift);
                 break;
 
             case 4:
