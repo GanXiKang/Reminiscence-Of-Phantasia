c1701a648bdaeb3c7ff0268a9b0196e103eeb7e0
GanXiKang <gan0612xk@hotmail.com>
2024-11-21 02:03:16 +0800

    feat ： delete code

--- Assets/Script/Story_Momotaro/StoryInteractableControl_Momotaro.cs
+++ Assets/Script/Story_Momotaro/StoryInteractableControl_Momotaro.cs
@@ -57,9 +57,6 @@ public class StoryInteractableControl_Momotaro : MonoBehaviour
     public Transform skillUIPosition;
     bool isSkill = false;
 
-    [Header("Effects")]
-    public GameObject effects;
-
     //Rotation
     private float totalRotation = 0f;
     private Quaternion initialRotation;
@@ -557,8 +554,6 @@ public class StoryInteractableControl_Momotaro : MonoBehaviour
                             StoryUIControl_Momotaro.isDialogue = true;
                             StoryDialogueControl_Momotaro._isAboveWho1 = _who;
                             StoryDialogueControl_Momotaro._textCount = 17;
-                            effects.SetActive(false);
-                            Destroy(effects, 1f);
                             break;
                     }
                     break;
