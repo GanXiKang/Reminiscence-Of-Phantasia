88c5e967abbf0b3f77cc3d4aeeb2dccb17d065f5
GanXiKang <gan0612xk@hotmail.com>
2024-11-24 16:32:37 +0800

    feat : 編寫code

--- Assets/Script/House/Bed/BedControl_House.cs
+++ Assets/Script/House/Bed/BedControl_House.cs
@@ -50,19 +50,6 @@ public class BedControl_House : MonoBehaviour
 
     void MoveToTarget()
     {
-        //print("YES");
-        //Vector3 direction = (bedPos.position - player.transform.position).normalized;
-        //Quaternion targetRotation = Quaternion.LookRotation(direction);
-        //player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, _rotateSpeed * Time.deltaTime);
-
-        //player.transform.position += direction * _moveSpeed * Time.deltaTime;
-
-        //if (Vector3.Distance(player.transform.position, bedPos.position) < 0.1f)
-        //{
-        //    print("Stop");
-        //    isMovingToBed = false;
-        //}
-
         Transform playerTransform = player.transform;
         CharacterController cc = player.GetComponent<CharacterController>();
 
@@ -76,9 +63,13 @@ public class BedControl_House : MonoBehaviour
         Vector3 move = direction * _moveSpeed * Time.deltaTime;
         cc.Move(move);
 
-        if (Vector3.Distance(playerTransform.position, bedPos.position) < 0.35f)
+        if (Vector3.Distance(playerTransform.position, bedPos.position) < 0.5f)
         {
             isMovingToBed = false;
+
+            Quaternion finalRotation = Quaternion.LookRotation(Vector3.forward);
+            playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, finalRotation, _rotateSpeed * Time.deltaTime);
+            playerTransform.rotation = finalRotation;
             print("Stop");
         }
     }
