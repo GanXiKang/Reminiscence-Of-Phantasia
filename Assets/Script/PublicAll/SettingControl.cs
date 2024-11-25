d2b98b8e59dbcf2fc3e3e3c2e664410af9fbcdd3
GanXiKang <gan0612xk@hotmail.com>
2024-11-25 21:57:03 +0800

    feat ： 更改

--- Assets/Script/PublicAll/SettingControl.cs
+++ Assets/Script/PublicAll/SettingControl.cs
@@ -8,42 +8,41 @@ public class SettingControl : MonoBehaviour
 {
     [Header("Musia")]
     public AudioSource BGM;
-    public AudioClip onClick, flipBook, open, save, leave;
- 
+    public AudioClip onClick, open, save, leave;
+
     [Header("UI")]
-    public Image bookUI;
+    public GameObject settingsUI;
+    public Image background;
+    public Sprite settingPage, operatePage;
     public Slider sliderBGM;
     public Toggle fullScreen;
-    public GameObject[] settingUI;
-    public Button[] labelColor;
-    public Sprite[] turnPage;
-
+    
     public static float volumeBGM = 0.2f;
     public static bool isFullS;
     public static bool isSettingActive = false;
-    public static int _page = 1;
-    public float _openSpeed = 1f;
-    bool isUIInteractable;
+    bool isOperate = false;
 
     void Start()
     {
         sliderBGM.value = volumeBGM;
         BGM.volume = volumeBGM;
         fullScreen.isOn = Screen.fullScreen;
-        isUIInteractable = true;
     }
 
     void Update()
     {
-        OpenUI();
+        SettingUI();
+        UIBackground();
+    }
 
-        if (isCanUseSetting())
+    void SettingUI()
+    {
+        settingsUI.SetActive(isSettingActive);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                BGM.PlayOneShot(open);
-                isSettingActive = !isSettingActive;
-            }
+            BGM.PlayOneShot(open);
+            isSettingActive = !isSettingActive;
         }
 
         if (Input.GetKeyDown(KeyCode.F11))
@@ -51,83 +50,34 @@ public class SettingControl : MonoBehaviour
             FullScreen(!isFullS);
         }
     }
-
-    bool isCanUseSetting()
+    void UIBackground()
     {
-        return true;
-    }
-
-    void OpenUI()
-    {
-        settingUI[0].SetActive(isSettingActive);
-
-        if (isSettingActive)
+        if (!isOperate)
         {
-            if (settingUI[0].GetComponent<RectTransform>().localScale.x < 1)
-            {
-                settingUI[0].GetComponent<RectTransform>().localScale += new Vector3(_openSpeed, _openSpeed, 0f) * Time.deltaTime;
-            }
-        }
-        else 
-        {
-            settingUI[0].GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 1f);
-        }
-    }
-    void UIInteractable()
-    {
-        if (isUIInteractable)
-        {
-            for (int i = 1; i < settingUI.Length; i++)
-            {
-                if (i == _page)
-                {
-                    settingUI[i].SetActive(true);
-                }
-                else
-                {
-                    settingUI[i].SetActive(false);
-                }
-            }
+            background.sprite = settingPage;
         }
         else
         {
-            for (int i = 1; i < settingUI.Length; i++)
-            {
-                settingUI[i].SetActive(false);
-            }
-        }
-    }
-    void LabelInteractable()
-    {
-        for (int i = 1; i < settingUI.Length; i++)
-        {
-            if (i == _page)
-            {
-                labelColor[i].interactable = false;
-            }
-            else
-            {
-                labelColor[i].interactable = true;
-            }
+            background.sprite = operatePage;
         }
     }
 
-    public void LabelGreen_Button()
-    {
-        _page = 1;
-        BGM.PlayOneShot(onClick);
-        StartCoroutine(TurnPageRight());
-    }
-    public void LabelBlue_Button()
-    {
-        _page = 2;
-        BGM.PlayOneShot(onClick);
-        StartCoroutine(TurnPageLeft());
-    }
     public void Close_Button()
     {
         BGM.PlayOneShot(onClick);
-        isSettingActive = !isSettingActive;
+        if (!isOperate)
+        {
+            isSettingActive = !isSettingActive;
+        }
+        else
+        {
+            isOperate = false;
+        }
+    }
+    public void Operate_Button()
+    {
+        BGM.PlayOneShot(onClick);
+        isOperate = true;
     }
     public void Save_Button()
     {
@@ -153,42 +103,5 @@ public class SettingControl : MonoBehaviour
     void GoToMenu()
     {
         SceneManager.LoadScene(0);
-    }
-
-    IEnumerator TurnPageLeft()
-    {
-        isUIInteractable = false;
-        UIInteractable();
-        bookUI.sprite = turnPage[1];
-        yield return new WaitForSeconds(0.2f);
-        BGM.PlayOneShot(flipBook);
-        bookUI.sprite = turnPage[2];
-        yield return new WaitForSeconds(0.2f);
-        bookUI.sprite = turnPage[4];
-        yield return new WaitForSeconds(0.2f);
-        LabelInteractable();
-        bookUI.sprite = turnPage[3];
-        yield return new WaitForSeconds(0.2f);
-        bookUI.sprite = turnPage[0];
-        isUIInteractable = true;
-        UIInteractable();
-    }
-    IEnumerator TurnPageRight()
-    {
-        isUIInteractable = false;
-        UIInteractable();
-        bookUI.sprite = turnPage[3];
-        yield return new WaitForSeconds(0.2f);
-        BGM.PlayOneShot(flipBook);
-        LabelInteractable();
-        bookUI.sprite = turnPage[4];
-        yield return new WaitForSeconds(0.2f);
-        bookUI.sprite = turnPage[2];
-        yield return new WaitForSeconds(0.2f);
-        bookUI.sprite = turnPage[1];
-        yield return new WaitForSeconds(0.2f);
-        bookUI.sprite = turnPage[0];
-        isUIInteractable = true;
-        UIInteractable();
     }
 }
