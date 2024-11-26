8fa731ab7e88737aab3efd6fff6472521196b30b
GanXiKang <gan0612xk@hotmail.com>
2024-11-27 00:50:24 +0800

    feat ： 測試結束

--- Assets/Script/House/Main/GameControl_House.cs
+++ Assets/Script/House/Main/GameControl_House.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameControl_House : MonoBehaviour
 {
     public static int _day = 1;
-    public static int _storyNum = 3;
+    public static int _storyNum = 0;
 
     void Start()
     {
