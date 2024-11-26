166fc070bdb9043e1069b2a18436971f45db1e9f
GanXiKang <gan0612xk@hotmail.com>
2024-11-27 01:12:54 +0800

    feat : 編寫code

--- Assets/Script/PublicStory/StoryHintDialogueControl.cs
+++ Assets/Script/PublicStory/StoryHintDialogueControl.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryHintDialogueControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Coroutine switchCoroutine;
 
-    // Update is called once per frame
+    public Image hint;
+    public Sprite spriteA, spriteB;
+    float _switchInterval = 1f;
+    bool isSwitching = false;
+
     void Update()
     {
-        
+        if (isDialogue())
+        {
+            if (!isSwitching)
+            {
+                isSwitching = true;
+                switchCoroutine = StartCoroutine(SwitchSprite());
+            }
+        }
+        else
+        {
+            isSwitching = false;
+            if (switchCoroutine != null)
+            {
+                StopCoroutine(switchCoroutine);
+                switchCoroutine = null;
+            }
+        }
+    }
+
+    bool isDialogue()
+    {
+        return StoryUIControl_Girl.isDialogue ||
+               StoryUIControl_Momotaro.isDialogue ||
+               StoryUIControl_Prince.isDialogue;
+    }
+
+    private IEnumerator SwitchSprite()
+    {
+        while (isSwitching)
+        {
+           hint.sprite = (hint.sprite == spriteA) ? spriteB : spriteA;
+            yield return new WaitForSeconds(_switchInterval);
+        }
     }
 }
