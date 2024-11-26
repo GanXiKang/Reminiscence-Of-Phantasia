00f9cc3eac5818cb9a5c4bdc5c07cf175a5107fc
GanXiKang <gan0612xk@hotmail.com>
2024-11-27 01:22:08 +0800

    feat ： 編寫code

--- Assets/Script/PublicStory/StoryOperateControl.cs
+++ Assets/Script/PublicStory/StoryOperateControl.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class StoryOperateControl : MonoBehaviour
 {
-    
+    CanvasGroup canvasGroup;
+    float _duration = 2f; 
+    float _delay = 10f;
+
+    public static bool isFadeOut = false;
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (isFadeOut)
+            StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        isFadeOut = false;
+
+        yield return new WaitForSeconds(_delay);
+
+        float elapsed = 0f;
+        float startAlpha = canvasGroup.alpha;
+
+        while (elapsed < _duration)
+        {
+            elapsed += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, 0, elapsed / _duration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
+        gameObject.SetActive(false);
+    }
 }
