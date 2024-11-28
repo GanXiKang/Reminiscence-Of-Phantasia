cba64a0fad8d2f39459ef830f26f2dbf2e888e1e
GanXiKang <gan0612xk@hotmail.com>
2024-11-28 23:20:42 +0800

    feat ： 測試結束

--- Assets/Script/House/Main/GameControl_House.cs
+++ Assets/Script/House/Main/GameControl_House.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GameControl_House : MonoBehaviour
 {
-    public static int _day = 1;
-    public static int _storyNum = 1;
+    public static int _day = 0;
+    public static int _storyNum = 0;
 
     void Start()
     {
@@ -22,16 +22,15 @@ public class GameControl_House : MonoBehaviour
             case 2:
                 DoorControl_House.isBird = true;
                 UIAboveObject_House.isAboveWorkbench = true;
+                UIAboveObject_House.isAboveBed = false;
                 InteractableControl_House.isColliderActive[1] = true;
                 UIControl_House.isDialogue = true;
                 DialogueControl_House._textCount = 30;
-
-                //úy‘á
-                BirdControl_House._goPointNum = 1;
                 break;
 
             case 3:
                 UIAboveObject_House.isAboveWorkbench = true;
+                UIAboveObject_House.isAboveBed = false;
                 InteractableControl_House.isColliderActive[1] = true;
                 InteractableControl_House.isColliderActive[4] = true;
                 InteractableControl_House.isColliderActive[5] = true;
