317bdaa4f3e392cba873e1765ebb96f33865c481
GanXiKang <gan0612xk@hotmail.com>
2024-11-28 21:50:32 +0800

    feat ： 調整

--- Assets/Script/House/Door/DoorControl_House.cs
+++ Assets/Script/House/Door/DoorControl_House.cs
@@ -65,14 +65,19 @@ public class DoorControl_House : MonoBehaviour
             DialogueControl_House.isCatTalk = true;
             DialogueControl_House._textCount = 25;
         }
-        else if(!InteractableControl_House.isBirdEntrust)
+        else if (!InteractableControl_House.isBirdEntrust)
         {
             isBird = true;
             InteractableControl_House.isBirdEntrust = true;
             UIControl_House.isDialogue = true;
             DialogueControl_House.isBirdTalk = true;
             DialogueControl_House._textCount = 28;
-        }else if( )
-
+        }
+        else if (InteractableControl_House.isBirdLeave)
+        {
+            InteractableControl_House.isBirdLeave = false;
+            UIControl_House.isDialogue = true;
+            DialogueControl_House._textCount = 10;
+        }
     }
 }
