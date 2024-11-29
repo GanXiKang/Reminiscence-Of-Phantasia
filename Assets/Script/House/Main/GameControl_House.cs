9ce87f5b1baa7cffc74b0fd2138cf7b3b297060c
GanXiKang <gan0612xk@hotmail.com>
2024-11-30 01:54:31 +0800

    feat ： 測試結束

--- Assets/Script/House/Main/GameControl_House.cs
+++ Assets/Script/House/Main/GameControl_House.cs
@@ -15,9 +15,8 @@ public class GameControl_House : MonoBehaviour
             case 1:
                 UIAboveObject_House.isAboveDoor = true;
                 InteractableControl_House.isColliderActive[2] = true;
-                DoorControl_House.isStore = true;
-                //UIControl_House.isDialogue = true;
-                //DialogueControl_House._textCount = 1;
+                UIControl_House.isDialogue = true;
+                DialogueControl_House._textCount = 1;
                 break;
 
             case 2:
