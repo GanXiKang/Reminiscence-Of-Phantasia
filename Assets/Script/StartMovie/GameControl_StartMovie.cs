a153980d754c86c84f7bf4135997b610b5466c42
GanXiKang <gan0612xk@hotmail.com>
2024-12-01 00:08:08 +0800

    feat ： 編寫code

--- Assets/Script/StartMovie/GameControl_StartMovie.cs
+++ Assets/Script/StartMovie/GameControl_StartMovie.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class GameControl_StartMovie : MonoBehaviour
 {
+    [Header("Video")]
+    public VideoPlayer videoPlayer;
+
     [Header("TransitionUI")]
     public GameObject transitionUI;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
 
     void Update()
     {
         transitionUI.SetActive(TransitionUIControl.isTransitionUIAnim_In || TransitionUIControl.isTransitionUIAnim_Out);
     }
 
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Skip_Button()
     {
-        
+        videoPlayer.Stop();
+        SceneManager.LoadScene(1);
     }
 }
