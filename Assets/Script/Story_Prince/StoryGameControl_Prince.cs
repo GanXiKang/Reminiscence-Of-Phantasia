cfc9c792734c754e721f782ebf4a6927232b9a9a
GanXiKang <gan0612xk@hotmail.com>
2024-12-01 20:36:00 +0800

    feat ： copy

--- Assets/Script/Story_Prince/StoryGameControl_Prince.cs
+++ Assets/Script/Story_Prince/StoryGameControl_Prince.cs
@@ -5,9 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class StoryGameControl_Prince : MonoBehaviour
 {
+    [Header("Texture")]
+    public Texture2D mouse1;
+    public Texture2D mouse2;
+    public Vector2 hotSpot = Vector2.zero;
+    bool isClick = false;
+
     void Start()
     {
         StartCoroutine(GoToMenu());
+    }
+
+    void Update()
+    {
+        MouseCursor();
+    }
+
+    void MouseCursor()
+    {
+        if (isClick)
+        {
+            Cursor.SetCursor(mouse2, hotSpot, CursorMode.Auto);
+        }
+        else
+        {
+            Cursor.SetCursor(mouse1, hotSpot, CursorMode.Auto);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isClick = true;
+            Invoke("FalseisClick", 0.5f);
+        }
+    }
+    void FalseisClick()
+    {
+        isClick = false;
     }
 
     IEnumerator GoToMenu()
