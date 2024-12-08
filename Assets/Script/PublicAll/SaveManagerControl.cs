b883db253ec09bbc24fb2c27c3eae24b9ec558be
GanXiKang <gan0612xk@hotmail.com>
2024-12-08 16:50:15 +0800

    feat : 編寫code

--- Assets/Script/PublicAll/SaveManagerControl.cs
+++ Assets/Script/PublicAll/SaveManagerControl.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+[System.Serializable]
+public class GameData
+{
+    public Vector3 playerPosition;
+    public int playerCoins;
+    public string currentSceneName;
+}
+
 public class SaveManagerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SaveManagerControl Instance { get; private set; }
+    private string saveFilePath;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            saveFilePath = Application.persistentDataPath + "/savefile.json";
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGame(GameData gameData)
     {
-        
+        string json = JsonUtility.ToJson(gameData);
+        File.WriteAllText(saveFilePath, json);
+        Debug.Log("Game saved to " + saveFilePath);
+    }
+
+    public GameData LoadGame()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+            return JsonUtility.FromJson<GameData>(json);
+        }
+        else
+        {
+            Debug.LogWarning("No save file found.");
+            return null;
+        }
     }
 }
