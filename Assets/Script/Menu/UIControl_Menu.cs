b7f3a66f56216aab191db7515f2662515ec5e43c
GanXiKang <gan0612xk@hotmail.com>
2024-12-09 19:43:14 +0800

    feat : 添加code

--- Assets/Script/Menu/UIControl_Menu.cs
+++ Assets/Script/Menu/UIControl_Menu.cs
@@ -31,6 +31,7 @@ public class UIControl_Menu : MonoBehaviour
 
         sliderBGM.value = SettingControl.volumeBGM;
         BGM.volume = SettingControl.volumeBGM;
+        sliderBGM.value = BGM.volume;
         fullScreen.isOn = Screen.fullScreen;
     }
 
