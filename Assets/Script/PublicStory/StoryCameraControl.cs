ab76071a80d97eac6cc3f4ff3ea3457f73eb0ac7
QiqiLuoluo <gan0612xk@hotmail.com>
2025-03-03 23:23:30 +0800

    feat ： 調整

--- Assets/Script/PublicStory/StoryCameraControl.cs
+++ Assets/Script/PublicStory/StoryCameraControl.cs
@@ -6,22 +6,17 @@ public class StoryCameraControl : MonoBehaviour
 {
     [Header("CameraMovement")]
     public float _smoothTime = 0.5f;
-    public static bool isFollow;
     private Vector3 velocity = Vector3.zero;
     Transform player;
 
     void Start()
     {
         player = GameObject.Find("Player").transform;
-        isFollow = true;
     }
 
     void LateUpdate()
     {
-        if (isFollow)
-        {
-            Vector3 _targetPosition = player.position;
-            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref velocity, _smoothTime);
-        }
+        Vector3 _targetPosition = player.position;
+        transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref velocity, _smoothTime);
     }
 }
