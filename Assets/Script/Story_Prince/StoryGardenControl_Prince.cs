e6cae7d5330407a1150da4f105ae4afa554f73fc
QiqiLuoluo <gan0612xk@hotmail.com>
2025-03-13 23:44:21 +0800

    feat : 添加

--- Assets/Script/Story_Prince/StoryGardenControl_Prince.cs
+++ Assets/Script/Story_Prince/StoryGardenControl_Prince.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class StoryGardenControl_Prince : MonoBehaviour
 {
     [Header("Now")]
+    public GameObject treeGrape_Now;
+    public GameObject treeCherry_Now;
 
     [Header("Past")]
+    public GameObject
 
     [Header("Future")]
-    public GameObject 
+    public GameObject treeGrape_Future;
+    public GameObject treeCherry_Future;
 
     void Start()
     {
