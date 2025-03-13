b299925aea490264c9d0b85bef371cc1664d61c1
QiqiLuoluo <gan0612xk@hotmail.com>
2025-03-13 23:18:56 +0800

    feat : 優化

--- Assets/Script/Story_Prince/StoryGardenControl_Prince.cs
+++ Assets/Script/Story_Prince/StoryGardenControl_Prince.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class StoryGardenControl_Prince : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
