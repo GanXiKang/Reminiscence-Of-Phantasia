2ba366905e787746d1bffbabd085d03f9c218fe3
QiqiLuoluo <gan0612xk@hotmail.com>
2025-03-20 23:35:13 +0800

    feat ： 調整

--- Assets/Script/Story_Prince/StoryGameControl_Prince.cs
+++ Assets/Script/Story_Prince/StoryGameControl_Prince.cs
@@ -42,7 +42,7 @@ public class StoryGameControl_Prince : MonoBehaviour
             PlotNpcActive();
 
         npc[2].SetActive(past.activeSelf);
-        npc[7].SetActive(past.activeSelf);
+        npc[6].SetActive(past.activeSelf);
         supplies_Now.SetActive(isSuppliesGameHard && !StoryUIControl_Prince.isSuppliesActive);
         supplies_Past.SetActive(isSuppliesGameEasy && !StoryUIControl_Prince.isSuppliesActive);
 
