fc8be01acf9fa2fe426ea04330dc14edd63223d3
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-01 22:55:14 +0800

    feat ： 調整回來

--- Assets/Script/Story_Prince/StorySuppliesGame_Prince.cs
+++ Assets/Script/Story_Prince/StorySuppliesGame_Prince.cs
@@ -48,7 +48,7 @@ public class StorySuppliesGame_Prince : MonoBehaviour
     public GameObject diagolue;
     public GameObject diagolueBG;
     public GameObject[] item;
-    public Image[] itemBlack;
+    public GameObject[] itemBlack;
     public GameObject[] itemCorrect;
     public Transform[] pointItem;
     public Sprite[] itemSprite;
