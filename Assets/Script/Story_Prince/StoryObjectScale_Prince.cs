9e23bf67c879787cb68daed92f3a05c89fb44101
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-02 01:08:32 +0800

    feat ： new

---
+++ Assets/Script/Story_Prince/StoryObjectScale_Prince.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryObjectScale_Prince : MonoBehaviour
+{
+    public Vector3 oriScale;
+    public Vector3 tarScale;
+    public float _speed;
+    bool isZoom = false;
+    float t;
+
+    void OnEnable()
+    {
+        isZoom = true;
+        t = 0f;
+        transform.localScale = oriScale;
+    }
+
+    void Update()
+    {
+        if (!isZoom) return;
+
+        t += 3f * Time.deltaTime;
+        transform.localScale = Vector3.Lerp(oriScale, tarScale, Mathf.Clamp01(t));
+
+        if (t >= 1f)
+            isZoom = false;
+    }
+
+    void OnDisable()
+    {
+        isZoom = false;
+        transform.localScale = oriScale;
+    }
+}
