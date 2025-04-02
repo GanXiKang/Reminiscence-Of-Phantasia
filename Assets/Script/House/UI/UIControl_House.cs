2271f8fe4cb2b5ae87d4af4ff0c5e37242523352
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-03 02:57:13 +0800

    feat ： 編寫code

--- Assets/Script/House/UI/UIControl_House.cs
+++ Assets/Script/House/UI/UIControl_House.cs
@@ -17,10 +17,32 @@ public class UIControl_House : MonoBehaviour
     public Image dayImage;
     public Sprite[] day;
     public Transform inPoint, outPoint;
+    float _speed = 2f;
+    bool isAppear = true;
 
     void Start()
     {
-        
+        StartCoroutine(DayUIMoveLoop());
+        dayImage.sprite = day[GameControl_House._day];
+    }
+
+    IEnumerator DayUIMoveLoop()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            Vector3 target = isAppear ? inPoint.position : outPoint.position;
+            while (Vector3.Distance(dayUI.transform.position, target) > 0.01f)
+            {
+                dayUI.transform.position = Vector3.MoveTowards(dayUI.transform.position, target, speed * Time.deltaTime);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(5f);
+
+            isAppear = !isAppear;
+        }
+
+        dayUI.SetActive(false);
     }
 
     void Update()
