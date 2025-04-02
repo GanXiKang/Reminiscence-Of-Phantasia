894ab3f11164033170159919635153f7f87a1aec
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-03 02:59:49 +0800

    feat : move code

--- Assets/Script/House/UI/DayUIControl_House.cs
+++ Assets/Script/House/UI/DayUIControl_House.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DayUIControl_House : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("DayUI")]
+    public GameObject dayUI;
+    public Image dayImage;
+    public Sprite[] day;
+    public Transform inPoint, outPoint;
+    float _speed = 2f;
+    bool isAppear = true;
+
     void Start()
     {
-        
+        StartCoroutine(DayUIMoveLoop());
+        dayImage.sprite = day[GameControl_House._day];
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DayUIMoveLoop()
     {
-        
+        for (int i = 0; i < 2; i++)
+        {
+            Vector3 target = isAppear ? inPoint.position : outPoint.position;
+            while (Vector3.Distance(dayUI.transform.position, target) > 0.01f)
+            {
+                dayUI.transform.position = Vector3.MoveTowards(dayUI.transform.position, target, _speed * Time.deltaTime);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(5f);
+
+            isAppear = !isAppear;
+        }
+
+        dayUI.SetActive(false);
     }
 }
--- Assets/Script/House/UI/UIControl_House.cs
+++ Assets/Script/House/UI/UIControl_House.cs
@@ -12,39 +12,6 @@ public class UIControl_House : MonoBehaviour
     public GameObject dialogueUI;
     public static bool isDialogue = false;
 
-    [Header("DayUI")]
-    public GameObject dayUI;
-    public Image dayImage;
-    public Sprite[] day;
-    public Transform inPoint, outPoint;
-    float _speed = 2f;
-    bool isAppear = true;
-
-    void Start()
-    {
-        StartCoroutine(DayUIMoveLoop());
-        dayImage.sprite = day[GameControl_House._day];
-    }
-
-    IEnumerator DayUIMoveLoop()
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            Vector3 target = isAppear ? inPoint.position : outPoint.position;
-            while (Vector3.Distance(dayUI.transform.position, target) > 0.01f)
-            {
-                dayUI.transform.position = Vector3.MoveTowards(dayUI.transform.position, target, _speed * Time.deltaTime);
-                yield return null;
-            }
-
-            yield return new WaitForSeconds(5f);
-
-            isAppear = !isAppear;
-        }
-
-        dayUI.SetActive(false);
-    }
-
     void Update()
     {
         transitionUI.SetActive(TransitionUIControl.isTransitionUIAnim_In || TransitionUIControl.isTransitionUIAnim_Out);
