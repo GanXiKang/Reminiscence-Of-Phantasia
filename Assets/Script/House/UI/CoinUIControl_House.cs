1e6d5f202d33bedaac6614296e43b9a076855802
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-03 03:16:00 +0800

    feat ： new script

---
+++ Assets/Script/House/UI/CoinUIControl_House.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinUIControl_House : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
