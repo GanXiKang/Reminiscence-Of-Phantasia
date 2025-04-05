ef6b6db0f536113ecc188a56a9678b2fbc138222
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-06 01:08:08 +0800

    feat ： 編寫code

--- Assets/Script/House/Main/InteractableControl_House.cs
+++ Assets/Script/House/Main/InteractableControl_House.cs
@@ -324,16 +324,12 @@ public class InteractableControl_House : MonoBehaviour
                 BirdControl_House.isIdle = true;
                 BirdControl_House._goPointNum = 4;
                 CatControl_House._goPointNum = 3;
-                Invoke("IsPlayerEndingPoint", 0.3f);
+                PlayerControl_House.isPlayerEndPoint = true;
 
                 UIControl_House.isDialogue = true;
                 DialogueControl_House.isBirdTalk = true;
                 DialogueControl_House._textCount = 49;
                 break;
         }
-    }
-    void IsPlayerEndingPoint()
-    {
-        PlayerControl_House.isPlayerEndPoint = true;
     }
 }
