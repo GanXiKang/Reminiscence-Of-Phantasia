be9ac89e502fad8b77d7f81d6ae8d17eb50edcca
QiqiLuoluo <gan0612xk@hotmail.com>
2025-04-08 22:17:38 +0800

    feat : delete

--- Assets/Script/Menu/UIControl_Menu.cs
+++ Assets/Script/Menu/UIControl_Menu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
 
 public class UIControl_Menu : MonoBehaviour
 {
